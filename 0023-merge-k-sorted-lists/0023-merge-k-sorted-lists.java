372c47eba4c7f05851310b1070447befbe88a171
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-08-31 23:10:08 -0400

    Time: 5 ms (58.12%), Space: 43.1 MB (77.13%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,19 +10,23 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+        //use priority queue to sort the list list 
        Queue<ListNode> minHeap = new PriorityQueue(new Comparator<ListNode>(){
            @Override
            public int compare(ListNode l1, ListNode l2){
                return l1.val -l2.val;
            }
        });
+        //insert all the linkedLists into minHeap it will sort in asc order
         for(ListNode lis:lists){
             if(lis!=null)
             minHeap.offer(lis);
         }
+        
         ListNode head = new ListNode(0);
         ListNode tail = head;
         while(!minHeap.isEmpty()){
+            // now pop the top linked list and take the first value into tail then push the linked list that starts from second element of poped list. when we push that into heap it will sort again in asc which givens us minimum value on the top
             tail.next = minHeap.poll();
             tail =tail.next;
             if(tail.next!=null) minHeap.offer(tail.next);
