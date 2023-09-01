6e4236c54419c08260af56ba2d539c22323f02d6
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-08-31 23:07:48 -0400

    Time: 4 ms (76.67%), Space: 43.4 MB (49.42%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,38 +10,25 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-       int size = lists.length;
-        int interval =1;
-        
-        while(interval < size){
-            
-            for(int i=0;i<size-interval;i+=2*interval){
-                lists[i]=mergeTwoSortedLists(lists[i],lists[i+interval]);
-            }
-            interval*=2;
+       Queue<ListNode> minHeap = new PriorityQueue(new Comparator<ListNode>(){
+           @Override
+           public int compare(ListNode l1, ListNode l2){
+               return l1.val -l2.val;
+           }
+       });
+        for(ListNode lis:lists){
+            if(lis!=null)
+            minHeap.offer(lis);
         }
-        return size>0?lists[0]:null;
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+        while(!minHeap.isEmpty()){
+            tail.next = minHeap.poll();
+            tail =tail.next;
+            if(tail.next!=null) minHeap.offer(tail.next);
+        }
+        return head.next;
         
     }
-    public ListNode mergeTwoSortedLists(ListNode l1, ListNode l2){
-        ListNode temp = new ListNode(0);
-        ListNode dummy = temp;
-        while(l1!=null && l2!=null){
-            if(l1.val<l2.val){
-                dummy.next=l1;
-                l1=l1.next;
-            }else{
-                dummy.next=l2;
-                l2=l2.next;
-            }
-            dummy = dummy.next;
-        }
-        if(l1!=null){
-            dummy.next = l1;
-        }
-        if(l2!=null){
-            dummy.next = l2;
-        }
-        return temp.next;
-    }
+    
 }
