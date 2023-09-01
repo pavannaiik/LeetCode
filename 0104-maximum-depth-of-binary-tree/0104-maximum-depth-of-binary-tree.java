0353687a89fd07bf8db37863c0b8a3f654ecd9e0
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-09-01 01:20:11 -0400

    Time: 1 ms (10.49%), Space: 43.6 MB (5.57%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -19,7 +19,19 @@ class Solution {
        if(root==null){
            return 0;
        }
-        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+       int level =0;
+        Queue<TreeNode>queue = new LinkedList();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int len = queue.size();
+            for(int i=0;i<len;i++){
+                TreeNode node = queue.poll();
+                if(node.left!=null) queue.offer(node.left);
+                if(node.right!=null) queue.offer(node.right);
+            }
+            level++;
+        }
+        return level;
         
     }
 }
