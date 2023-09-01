1f8504391f7a1e2b8a14c0f5347080085a4524aa
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-08-31 20:18:06 -0400

    Time: 52 ms (73.14%), Space: 116.3 MB (85.85%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,10 +1,14 @@
 class LRUCache {
     
+    //size to keep know the capacity 
     private int size;
+    //create a hashmap which stores keys and Nodes
     HashMap<Integer,Node>map;
+    //since we need doubly linkedlist create two nodes front and rear
     Node front;
     Node rear;
     public LRUCache(int capacity) {
+        //initialize size , map, front ,rear and point front and rear each other
         size=capacity;
         map = new HashMap<>();
         front =new Node(-1,-1);
@@ -14,25 +18,22 @@ class LRUCache {
     }
     
     public int get(int key) {
+        // in get we check the key exist in map if it exist then we remove it from map and add the node to the front of the doublylinked list so  here remove method takes care of removing it from ll and map
         if(map.containsKey(key)){
             Node cur = map.get(key);
             remove(cur);
             insert(cur);
             return cur.val;
         }else{
+            //else we return -1 as mentioned in question
             return -1;
         }
         
     }
     public void insert(Node node){
         
-        // Node headNext = head.next;
-        // headNext.prev = node;
-        // node.next = headNext;
-        // head.next = node;
-        // headNext.prev = node;
-        // node.prev = head;
-
+        // in insert we insert key and node into map first 
+        // we have three nodes now so map each other 
         map.put(node.key,node);
         Node frontNext = front.next;
         front.next= node;
@@ -41,22 +42,28 @@ class LRUCache {
         frontNext.prev = node;
     }
     
+    
     public void put(int key, int value) {
+        // in put we check if the key contains in map if yes then we remove it from map add ll
+        // then we add it map and ll at the front
         if(map.containsKey(key)){
            remove(map.get(key));
         }
+        // if map size is full then we remove the last element which is LRU which is present at rear.prev
         if(map.size()== size){
                 remove(rear.prev);
             }
-        
+        // insert at the front
         insert(new Node(key,value));
     }
+    // remove we remove from map and we map node last and next node with each other
     public void remove(Node cur){
         map.remove(cur.key);
         cur.prev.next = cur.next;
         cur.next.prev = cur.prev;
     }
     
+    // create a class with node requirement
     private class Node{
         int key;
         int val;
