a77d58a0cf8e20a7f42271b043d02301b76edeea
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-09-04 17:15:08 -0400

    Time: 0 ms (100.00%), Space: 40.3 MB (94.11%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -4,6 +4,8 @@ class Solution {
         if(s!=null && n!=0 && s.charAt(0)=='0') return 0;
         int twoback =1;
         int oneback = s.charAt(0)=='0'?0:1;
+        // we have to go till last element thats why we are going till n+1 
+        // we are only check i-1 not i
         for(int i=2;i<n+1;i++){
             int current = s.charAt(i-1)=='0'?0:oneback;
             if(s.charAt(i-2)=='1' || s.charAt(i-2)=='2' && s.charAt(i-1)- '0' < 7){
