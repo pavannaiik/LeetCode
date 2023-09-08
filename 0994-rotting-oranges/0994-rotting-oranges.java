7560eb348a3975f636a0964b1f70a37f7606afec
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-09-08 03:40:04 -0400

    Time: 3 ms (21.25%), Space: 41 MB (75.34%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,29 +1,39 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int ans =2;
+        int[] dr ={0,0,1,-1};
+        int[] dc ={1,-1,0,0};
+        int count_fresh=0;
+        Queue<int[]> queue = new LinkedList<>();
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==2){
-                    dfs(grid,i,j,2);
+                    queue.offer(new int[]{i,j});
+                }else if(grid[i][j]==1){
+                    count_fresh++;
                 }
             }
         }
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1) return -1;
-                 ans = Math.max(ans,grid[i][j]);
+        if(count_fresh==0) return 0;
+        int min=0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            min++;
+            for(int i=0;i<size;i++){
+                int[] curPoint = queue.poll();
+                int row = curPoint[0];
+                int col = curPoint[1];
+                for(int p =0;p<4;p++){
+                        if(row+dr[p] <0 || row+dr[p] >= grid.length || col+dc[p] <0 ||col+dc[p] >= grid[0].length || grid[row+dr[p]][col+dc[p]]!=1 ){
+                            continue;
+                        }
+                        grid[row+dr[p]][col+dc[p]]=2;
+                        queue.offer(new int[]{row+dr[p],col+dc[p]});
+                        count_fresh--;
+                }
             }
+            
         }
-        return ans-2;
-    }
-    public void dfs(int[][] grid, int i, int j,int time){
-        if(i<0 || j<0 || i>=grid.length|| j>=grid[0].length || grid[i][j]==0) return ;
-        if(grid[i][j]>1 && grid[i][j]<time) return;
-        grid[i][j]=time;
-        dfs(grid,i,j+1,time+1);
-        dfs(grid,i,j-1,time+1);
-        dfs(grid,i-1,j,time+1);
-        dfs(grid,i+1,j,time+1);
         
+        return count_fresh !=0? -1:min-1;
     }
 }
