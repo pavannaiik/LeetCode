7bfa22464a9dd79803263027c185d4cd9b5efb3d
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-09-19 11:37:31 -0400

    Time: 0 ms (100.00%), Space: 41.2 MB (17.78%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double myPow(double x, int n) {
+        
+        return myPower(x,(long)n);
+    }
+    public double myPower(double x,long n){
+        if(n==0) return 1;
+        if(n<0){
+            return 1.0 / myPower(x,-n);
+        }
+        if(n%2 ==0){
+            return myPower(x*x, n/2);
+        }else{
+            return x*myPower(x*x,(n-1)/2);
+        }
+    }
+}
