caf6fca92fc57cd4488e380479fb85c9e86e232f
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-09-19 08:56:09 -0400

    Time: 0 ms (100.00%), Space: 41 MB (25.78%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,6 +1,7 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
+        // the idea is to shift the i by 31 positions
         int ans  = 0;
         int power =31;
         while(n !=0){
