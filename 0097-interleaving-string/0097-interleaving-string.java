a24fd5aef02ef3d429231a23008f286ef470eb6c
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-09-22 18:52:06 -0400

    Time: 4 ms (50.14%), Space: 40.5 MB (67.78%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,30 +1,20 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if(s1.length()+s2.length()!= s3.length()){
-            return false;
+        int m = s1.length(),n =s2.length(),p =s3.length();
+        if(m+n !=p) return false;
+        boolean[] dp = new boolean[n+1];
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0 && j==0) dp[j]=true;
+                else if(i==0){
+                    dp[j] = dp[j-1] && s2.charAt(j-1)==s3.charAt(i+j-1);
+                }else if(j==0){
+                    dp[j] = dp[j] && s1.charAt(i-1)==s3.charAt(i+j-1);
+                }else{
+                    dp[j] = (dp[j-1] && s2.charAt(j-1)==s3.charAt(i+j-1)) || (dp[j] && s1.charAt(i-1)==s3.charAt(i+j-1));
+                }
+            }
         }
-        int[][] cache = new int[s1.length()][s2.length()];
-        for(int[] arr: cache){
-            Arrays.fill(arr,-1);
-        }
-        return isInterleaving(s1,0,s2,0,s3,0,cache);
-    }
-    public boolean isInterleaving(String s1,int i, String s2,int j,String s3,int k,int[][] matrix){
-       
-        if(i==s1.length()){
-            return s2.substring(j).equals(s3.substring(k));
-        }
-        if(j==s2.length()){
-            return s1.substring(i).equals(s3.substring(k));
-        }
-        if(matrix[i][j] >= 0){
-            return matrix[i][j]==1?true:false;
-        }
-        boolean ans =false;
-        if(s1.charAt(i)==s3.charAt(k) && isInterleaving(s1,i+1,s2,j,s3,k+1,matrix) || s2.charAt(j)==s3.charAt(k) && isInterleaving(s1,i,s2,j+1,s3,k+1,matrix) ){
-            ans =true;
-        }
-        matrix[i][j]=ans?1:0;
-        return ans;
+        return dp[n];
     }
 }
