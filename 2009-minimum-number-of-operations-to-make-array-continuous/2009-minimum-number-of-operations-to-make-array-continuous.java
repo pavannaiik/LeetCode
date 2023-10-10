0e078eaa201be052d78a500d5d402b4af955e85c
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-10-10 07:08:09 -0400

    Time: 36 ms (99.02%), Space: 58.7 MB (16.67%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
@@ -1,26 +1,29 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int n =nums.length;
-        int ans = n;
-        HashSet<Integer>set=new HashSet<>();
-        for(int i:nums){
-            if(!set.contains(i))
-                set.add(i);
-        }
-        int[] res = new int[set.size()];
-        int k=0;
-        for(int i:set){
-            res[k++]=i;
-        }
-        Arrays.sort(res);
-        int j=0;
-         for (int i = 0; i < res.length; i++) {
-            while (j < res.length && res[j] < res[i] + n) {
-                j++;
+        int N = nums.length;
+        int ans = N;
+        int j = 0;
+
+        // Sort the array
+        Arrays.sort(nums);
+
+        // Remove duplicates to only keep unique elements
+        int M = 1;
+        for (int i = 1; i < N; ++i) {
+            if (nums[i] != nums[i - 1]) {
+                nums[M] = nums[i];
+                M++;
             }
-            int count = j - i;
-            ans = Math.min(ans, n - count);
         }
+
+        for (int i = 0; i < M; ++i) {
+            while (j < M && nums[j] < nums[i] + N) {
+                ++j;
+            }
+            // The length of this subarray is `j - i`. We need to replace `N - j + i` elements to make it continuous.
+            ans = Math.min(ans, N - j + i);
+        }
+
         return ans;
         
     }
