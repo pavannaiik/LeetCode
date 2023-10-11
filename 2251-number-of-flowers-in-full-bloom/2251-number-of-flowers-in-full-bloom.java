7775c04f5611ac27b32553e7abc6e239344506ae
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-10-11 04:23:22 -0400

    Time: 21 ms (100.00%), Space: 71.7 MB (60.77%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.java
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.java
@@ -1,39 +1,52 @@
 class Solution {
-    public int[] fullBloomFlowers(int[][] flowers, int[] people) {
-        List<Integer> starts = new ArrayList();
-        List<Integer> ends = new ArrayList();
-        
-        for (int[] flower: flowers) {
-            starts.add(flower[0]);
-            ends.add(flower[1] + 1);
-        }
-        
-        Collections.sort(starts);
-        Collections.sort(ends);
-        int[] ans = new int[people.length];
-        
-        for (int index = 0; index < people.length; index++) {
-            int person = people[index];
-            int i = binarySearch(starts, person);
-            int j = binarySearch(ends, person);
-            ans[index] = i - j;
-        }
-        
-        return ans;
-    }
+public int[] fullBloomFlowers(int[][] flowers, int[] people) {
+
+  int m = flowers.length;
+  int[] starts = new int[m], ends = new int[m];
+  for (int i = 0; i < m; i++) {
+      starts[i] = flowers[i][0];
+      ends[i] = flowers[i][1];
+  }
+  Arrays.sort(starts);
+  Arrays.sort(ends);
+
+  int n = people.length;
+  
+  int[] indices = new int[n];
+  for (int i = 0; i < n; i++) 
+      indices[i] = i;
+  
+  quickSort(indices, people, 0, n - 1);
+  
+  int[] result = new int[n];
+  int index1 = 0, index2 = 0;
+  for (int index:indices) {
+      int arrival = people[index];
+      while ( index1 < m && starts[index1] <= arrival) index1++;
+      while ( index2 < m && ends[index2] < arrival) index2++;  
+      result[index] = index1 - index2;
+  }
+  return result;
+
+}
+
+public void quickSort(int[] indices, int[] people, int low, int high) {
     
-    public int binarySearch(List<Integer> arr, int target) {
-        int left = 0;
-        int right = arr.size();
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (target < arr.get(mid)) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
+    if (low >= high) return;
+
+    int left = low -1, right = high + 1, mid = low + high >> 1;
+    int pivot = people[indices[mid]];
+
+    while (left  < right) {
+        while (people[indices[++left]] < pivot) continue;
+        while (people[indices[--right]] > pivot) continue;
+        if (left < right) {
+            int temp = indices[left];
+            indices[left] = indices[right];
+            indices[right] = temp;
         }
-        
-        return left;
     }
+    quickSort(indices, people, low, right); 
+    quickSort(indices, people, right + 1, high);
+  }
 }
