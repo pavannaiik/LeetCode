d356ccb39e934949a26bf2886add656af2b016ec
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-10-19 19:53:32 -0400

    Time: 2 ms (62.23%), Space: 41.1 MB (7.93%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.java
+++ 0874-backspace-string-compare/0874-backspace-string-compare.java
@@ -1,9 +1,8 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        int i=s.length()-1;
-        int j= t.length()-1;
-        String sb = "";
-        String tb ="";
+        return build(s).equals(build(t));
+    }
+    public String build(String s){
         Stack<Character> stack = new Stack<>();
         for(int p=0;p<s.length();p++){
             if(s.charAt(p)=='#' && !stack.isEmpty()){
@@ -13,22 +12,6 @@ class Solution {
                 stack.push(s.charAt(p));
             }
         }
-        while(!stack.isEmpty()){
-            sb = stack.peek() + sb;
-            stack.pop();
-        }
-        for(int p=0;p<t.length();p++){
-            if(t.charAt(p)=='#' && !stack.isEmpty()){
-                stack.pop();
-            }else{
-                if(t.charAt(p)!='#')
-                stack.push(t.charAt(p));
-            }
-        }
-        while(!stack.isEmpty()){
-            tb = stack.peek() + tb;
-            stack.pop();
-        }
-        return sb.equals(tb);
+        return String.valueOf(stack);
     }
 }
