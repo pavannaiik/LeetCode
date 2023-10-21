0c734b6f040701614ce88685baf46913525814d7
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-10-21 18:25:29 -0400

    Time: 1635 ms (87.38%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -2,3 +2,4 @@
 select e.name, b.bonus from employee as e left join 
 bonus as b on e.empId = b.empId
 where ifnull(b.bonus,0) < 1000;
+#ifnull returns first value if the value is not null and returns second value if its null
