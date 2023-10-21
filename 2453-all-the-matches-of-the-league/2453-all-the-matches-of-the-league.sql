f4e6101f99b8e4ea7ab01aa87c7876eef307c7f5
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-10-21 18:30:43 -0400

    Time: 1034 ms (50.95%), Space: 0B (100%) - LeetHub

--- 2453-all-the-matches-of-the-league/2453-all-the-matches-of-the-league.sql
+++ 2453-all-the-matches-of-the-league/2453-all-the-matches-of-the-league.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select t1.team_name as home_team ,t2.team_name as away_team 
-from teams t1, teams t2
-where t1.team_name <> t2.team_name;
+from teams t1 join teams t2
+on t1.team_name != t2.team_name;
