3f6997034ad859415a7a2762b60902d133fd2cf6
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-10-22 15:06:12 -0400

    Time: 48 ms (80.14%), Space: 63.8 MB (5.8%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,28 +1,60 @@
-public class Solution {
+class Solution {
+    
     public int largestRectangleArea(int[] heights) {
-        int maxarea = 0;
-       int[] left = new int[heights.length];
-        int[] right = new int[heights.length];
-        left[0]=-1;
-        right[heights.length-1]=heights.length;
-        for(int i=1;i<heights.length;i++){
-            int p =i-1;
-            while(p>=0 && heights[i]<=heights[p]){
-                p=left[p];
+        int n = heights.length;
+        int[] segment = buildSegmentTree(heights);
+        return divideConquer(heights, 0, n - 1, segment);        
+    }
+    
+    private static int divideConquer(int[] height, int l, int h, int[] segment) {
+        if(l <= h) {
+            if(l == h) return height[l] * 1;
+            int minIndex = query(segment, height, l, h);
+            int currArea = height[minIndex] * (h - l + 1);
+            int leftArea = divideConquer(height, l, minIndex - 1, segment);
+            int rightArea = divideConquer(height, minIndex + 1, h, segment);
+            return Math.max(Math.max(currArea, leftArea), rightArea);
+        }
+        return 0;
+    }
+    
+    private static int[] buildSegmentTree(int[] heights) {
+        int n = heights.length;
+        int[] segment = new int[2 * n];
+        for(int i = n - 1, j = 2 * n - 1; i >= 0; i--, j--) {
+            segment[j] = i;
+        }
+        for(int i = n - 1; i > 0; i--) {
+            if(heights[segment[2 * i]] < heights[segment[2 * i + 1]]) segment[i] = segment[2 * i];
+            else segment[i] = segment[2 * i + 1];
+        }
+        return segment;
+    }
+    
+    private static int query(int[] segment, int[] heights, int i, int j) {
+        int n = heights.length;
+        int p = i + n;
+        int q = j + n;
+        int min = Integer.MAX_VALUE;
+        int index = -1;
+        while(p <= q) {
+            if(p % 2 == 1) {
+                if(heights[segment[p]] < min) {
+                    min = heights[segment[p]];
+                    index = segment[p];
+                }
+                p++;
             }
-            left[i]=p;
-        }
-        for(int i=heights.length-2;i>=0;i--){
-           int p=i+1;
-            while(p<heights.length && heights[i]<=heights[p]){
-                p=right[p];
+            if(q % 2 == 0) {
+                if(heights[segment[q]] < min) {
+                    min = heights[segment[q]];
+                    index = segment[q];
+                }
+                q--;
             }
-            right[i]=p;
-            
+            p = p >> 1;
+            q = q >> 1;
         }
-        for(int i=0;i<heights.length;i++){
-            maxarea = Math.max(maxarea,heights[i]*(right[i]-left[i]-1));
-        }
-        return maxarea;
+        return index;
     }
 }
