30ca9381c33958ac068cd9d7ac8d65f67ce7652b
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-10-23 16:11:52 -0400

    Time: 1 ms (76.18%), Space: 40.1 MB (6.4%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,9 +1,19 @@
-class Solution {
-    public boolean isPowerOfFour(int n) {
-        if(n==0) return false;
-        while(n%4 ==0){
-            n = n/4;
+class Power{
+    private int num =15;
+    ArrayList<Integer>list = new ArrayList<>();
+    Power(){
+        int lastNum = 1;
+        list.add(lastNum);
+        for(int i=1;i<=num;i++){
+            lastNum = 4*lastNum;
+            list.add(lastNum);
         }
-        return n==1;
+    }
+}
+
+class Solution {
+    public static Power p = new Power();
+    public boolean isPowerOfFour(int n) {
+        return p.list.contains(n);
     }
 }
