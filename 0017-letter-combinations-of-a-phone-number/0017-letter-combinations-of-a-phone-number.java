ce83d46faba93cae9aba993aa92ea3f1def6429d
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2023-11-03 15:02:13 -0400

    Time: 1 ms (79.6%), Space: 41.2 MB (37.78%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,25 +1,22 @@
+import java.util.Map;
 class Solution {
-    private List<String> combinations = new ArrayList<>();
-    private Map<Character, String> letters = Map.of(
-        '2', "abc", '3', "def", '4', "ghi", '5', "jkl", 
-        '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
-    private String phoneDigits;
+    private List<String>result = new ArrayList<>();
+    private final Map<Character,String>map = Map.of('2',"abc",'3',"def",'4',"ghi",'5',"jkl",'6',"mno",'7',"pqrs",'8',"tuv",'9',"wxyz");
     public List<String> letterCombinations(String digits) {
-        if(digits.length()==0) return combinations;
-        backtrack(digits,0,new StringBuilder());
-        return combinations;
+        if(digits.length()==0) return result;
+        backtrack(digits, 0, new StringBuilder());
+        return result;
     }
-    public void backtrack(String s,int index,StringBuilder res){
-        if(res.length()==s.length()){
-            combinations.add(res.toString());
+    public void backtrack(String digits, int index, StringBuilder sb){
+        if(sb.length() == digits.length()){
+            result.add(sb.toString());
             return;
         }
-            String temp = letters.get(s.charAt(index));
-            for(char ch:temp.toCharArray()){
-                res.append(ch);
-                backtrack(s,index+1, res);
-                res.deleteCharAt(res.length()-1);
-            }
-        return;
+        String temp = map.get(digits.charAt(index));
+        for(char ch: temp.toCharArray()){
+            sb.append(ch);
+            backtrack(digits, index+1, sb);
+            sb.deleteCharAt(sb.length()-1);
+        }
     }
 }
