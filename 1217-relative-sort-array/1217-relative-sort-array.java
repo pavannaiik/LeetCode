b1430d9be3992882620ff6b16cbaed70dfb8da14
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-06-11 17:11:30 -0400

    Time: 0 ms (100%), Space: 42 MB (61.14%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.java
+++ 1217-relative-sort-array/1217-relative-sort-array.java
@@ -1,32 +1,28 @@
 class Solution {
-
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        int maxElement = Arrays.stream(arr1).max().orElse(0);
-        int[] count = new int[maxElement + 1];
-
-        // Count occurrences of each element
-        for (int element : arr1) {
-            count[element]++;
+        int[] freq = new int[1001];
+        
+        for(int i = 0; i < arr1.length; i++) {
+            freq[arr1[i]]++;
         }
+        
+        int[] ans = new int[arr1.length];
 
-        List<Integer> result = new ArrayList<>();
-        // Add elements as per relative order
-        for (int value : arr2) {
-            while (count[value] > 0) {
-                result.add(value);
-                count[value]--;
+        int idx = 0;
+        for(int num: arr2) {
+            while(freq[num] > 0) {
+                ans[idx++] = num;
+                freq[num]--;
             }
         }
-
-        // Add remaining elements in ascending order
-        for (int num = 0; num <= maxElement; num++) {
-            while (count[num] > 0) {
-                result.add(num);
-                count[num]--;
+        
+        for(int i = 0; i < freq.length; i++) {
+            while(freq[i] > 0) {
+                ans[idx++] = i;
+                freq[i]--;
             }
         }
-
-        // Convert ArrayList to array
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        
+        return ans;
     }
 }
