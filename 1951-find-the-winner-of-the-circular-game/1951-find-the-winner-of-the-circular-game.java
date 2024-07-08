fc483897d58e4456c5d0020c5f0932d42149725e
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-07-08 17:25:17 -0400

    Time: 0 ms (100%), Space: 40.3 MB (73.92%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,32 +1,10 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-       HashSet<Integer>set = new HashSet<>();
-       for(int i=1;i<=n;i++){
-        set.add(i);
-       }
-       int t=1;
-       int i=1;
-       while(i%n <= n){
-        if(set.size()==1){
-            break;
+        int ans = 0;
+        for (int i = 2; i <= n; i++) {
+            ans = (ans + k) % i;
         }
-        if((i%n==0 && set.contains(n)) || set.contains(i%n)){
-            if(t==k){
-                if(i%n==0)
-                set.remove(n);
-                else set.remove(i%n);
-                t=1;
-            }else{
-                t++;
-            }
-        }
-            i++;
-       }
-       int ans =0;
-       for(int p:set){
-        ans =p;
-        break;
-       }
-       return ans;
+        // add 1 to convert back to 1 indexing
+        return ans + 1;
     }
 }
