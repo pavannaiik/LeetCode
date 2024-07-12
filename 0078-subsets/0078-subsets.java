259c1a1a566340f3adafa7212bd28b3a275ad5f2
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-07-12 03:35:32 -0400

    Time: 1 ms (52.04%), Space: 43 MB (7.4%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,18 +1,18 @@
 class Solution {
-    
+    List<List<Integer>>ans;
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>>ans = new ArrayList<>();
-        recur(nums, 0, new ArrayList<>(), ans);
+        ans = new ArrayList<>();
+        recur(nums, 0, new ArrayList<>());
         return ans;
     }
-    public void recur(int[] nums, int index, List<Integer>temp, List<List<Integer>>ans){
+    public void recur(int[] nums, int index, List<Integer>temp){
         ans.add(new ArrayList<>(temp));
         if(index >= nums.length){
             return;
         }
         for(int i=index;i<nums.length;i++){
             temp.add(nums[i]);
-            recur(nums, i+1, temp, ans);
+            recur(nums, i+1, temp);
             temp.remove(temp.size()-1);
         }
     }
