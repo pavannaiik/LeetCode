51ab23e13cfe97c332fc898eb988c4fa2e602d44
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-07-14 12:44:55 -0400

    Time: 5 ms (57.69%), Space: 41.9 MB (36.43%) - LeetHub

---
+++ 0726-number-of-atoms/0726-number-of-atoms.java
@@ -0,0 +1,53 @@
+class Solution {
+    public String countOfAtoms(String formula) {
+        TreeMap<String, Integer> countMap = new TreeMap<>();
+        Stack<TreeMap<String, Integer>> stack = new Stack<>();
+        int n = formula.length();
+        
+        int i = 0;
+        while (i < n) {
+            if (formula.charAt(i) == '(') {
+                stack.push(new TreeMap<>(countMap));
+                countMap.clear();
+                i++;
+            } else if (formula.charAt(i) == ')') {
+                Map<String, Integer> tempMap = countMap;
+                countMap = stack.pop();
+                i++;
+                int start = i;
+                while (i < n && Character.isDigit(formula.charAt(i))) {
+                    i++;
+                }
+                int multiplicity = (start < i) ? Integer.parseInt(formula.substring(start, i)) : 1;
+                for (String key : tempMap.keySet()) {
+                    countMap.put(key, countMap.getOrDefault(key, 0) + tempMap.get(key) * multiplicity);
+                }
+            } else {
+                int start = i;
+                i++;
+                while (i < n && Character.isLowerCase(formula.charAt(i))) {
+                    i++;
+                }
+                String name = formula.substring(start, i);
+                start = i;
+                while (i < n && Character.isDigit(formula.charAt(i))) {
+                    i++;
+                }
+                int multiplicity = (start < i) ? Integer.parseInt(formula.substring(start, i)) : 1;
+                countMap.put(name, countMap.getOrDefault(name, 0) + multiplicity);
+            }
+        }
+        
+        StringBuilder str = new StringBuilder();
+        for (String name : countMap.keySet()) {
+            str.append(name);
+            int count = countMap.get(name);
+            if (count > 1) {
+                str.append(count);
+            }
+        }
+        
+        return str.toString();
+
+    }
+}
