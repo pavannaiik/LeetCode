31f665b26e0bcdcb1ac322fe955d9434c8d5ac02
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-07-15 02:45:35 -0400

    Time: 0 ms (100%), Space: 40.4 MB (46.39%) - LeetHub

--- 0796-reaching-points/0796-reaching-points.java
+++ 0796-reaching-points/0796-reaching-points.java
@@ -1,16 +1,13 @@
 class Solution {
     public boolean reachingPoints(int sx, int sy, int tx, int ty) {
-       while(tx >= sx && ty >= sy){
-            if(tx==ty) break;
-            if(tx > ty){
-                if( ty > sy) tx %= ty;
-                else return (tx-sx)%ty==0;
-            }else{
-               if(tx > sx) ty %= tx;
-               else return (ty-sy)%tx ==0;
-            }
-       }
-       return( tx == sx && ty == sy);
+       while(tx > sx && ty > sy){
+            if(tx>ty) tx=tx%ty;
+            else ty=ty%tx;
+        }
+       // we check extra conditions because we might end up 
+       //a lower value than sx, sy while doing modulo
+        return (sx==tx && sy<=ty && (ty-sy)%sx==0) || 
+                (sy==ty && sx<=tx && (tx-sx)%sy==0); 
     }
     
 }
