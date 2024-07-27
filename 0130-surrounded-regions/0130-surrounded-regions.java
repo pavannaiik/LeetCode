ad8667709fcec3962812f5aab7f115001e43f74e
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-07-27 13:40:09 -0400

    Time: 2 ms (86.45%), Space: 45.5 MB (25.31%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,36 +1,35 @@
 class Solution {
     public void solve(char[][] board) {
-        int n = board.length;
+       int n = board.length;
         int m = board[0].length;
         //go through the first colum and last column
         for(int i=0;i<n;i++){
-            dfs(board,i,0,n,m);
-            dfs(board,i,m-1,n,m); 
+            dfs(board,i,0);
+            dfs(board,i,m-1); 
         }
         //go through first row and last row
         for(int j=0;j<m;j++){
-            dfs(board,0,j,n,m);
-            dfs(board,n-1,j,n,m); 
+            dfs(board,0,j);
+            dfs(board,n-1,j); 
         }
-        // make all O to X and * to O 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j]=='*'){
                     board[i][j]='O';
-                }else{
+                }else if(board[i][j]=='O'){
                     board[i][j]='X';
                 }
             }
         }
     }
-    public void dfs(char[][] board,int i,int j,int n,int m){
-        if(i<0 || i>=n ||j<0 ||j>=m || board[i][j]=='X' || board[i][j]=='*'){
+    public void dfs(char[][] board, int i, int j){
+        if(i<0 || i >= board.length || j <0 || j>=board[0].length || board[i][j]=='X' || board[i][j]=='*'){
             return;
         }
-        board[i][j] = '*';
-        dfs(board,i+1,j,n,m);
-        dfs(board,i-1,j,n,m);
-        dfs(board,i,j+1,n,m);
-        dfs(board,i,j-1,n,m);
+        board[i][j] ='*';
+        dfs(board, i, j+1);
+        dfs(board, i, j-1);
+        dfs(board, i+1, j);
+        dfs(board, i-1, j);
     }
 }
