cbe1c3bebb801b09425d09cf8a2c662643b46542
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-07-30 15:42:40 -0400

    Time: 20 ms (89.16%), Space: 45.5 MB (40.38%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.java
@@ -1,16 +1,20 @@
 class Solution {
+
     public int minimumDeletions(String s) {
-        Stack<Character>stack = new Stack<>();
-        int ans =0;
-        for(char c:s.toCharArray()){
-            if(!stack.isEmpty() && stack.peek()=='b' && c=='a'){
-                stack.pop();
-                ans++;
-            }else{
-                stack.push(c);
+        int n = s.length();
+        int minDeletions = 0;
+        int bCount = 0;
+
+        // minDeletions variable represents dp[i]
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'b') {
+                bCount++;
+            } else {
+                // Two cases: remove 'a' or keep 'a'
+                minDeletions = Math.min(minDeletions + 1, bCount);
             }
         }
-        return ans;
 
+        return minDeletions;
     }
 }
