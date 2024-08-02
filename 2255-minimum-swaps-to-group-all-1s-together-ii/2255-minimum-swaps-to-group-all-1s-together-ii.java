b5b72d16b8cdf29dd86d4b81a9859d88d1f4a95e
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-08-02 19:55:08 -0400

    Time: 4 ms (97.46%), Space: 59.1 MB (58.38%) - LeetHub

---
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    public int minSwaps(int[] nums) {
+        // Initialize minimum swaps to a large value
+        int minimumSwaps = Integer.MAX_VALUE;
+
+        // Calculate the total number of 1s in the array
+        int totalOnes = 0;
+        for (int num : nums) {
+            totalOnes += num;
+        }
+
+        // Initialize the count of 1s in the current window
+        int onesCount = nums[0];
+        int end = 0;
+
+        // Slide the window across the array
+        for (int start = 0; start < nums.length; ++start) {
+            // Adjust onesCount by removing the element that is sliding out of
+            // the window
+            if (start != 0) {
+                onesCount -= nums[start - 1];
+            }
+
+            // Expand the window to the right until it reaches the desired size
+            while (end - start + 1 < totalOnes) {
+                end++;
+                onesCount += nums[end % nums.length];
+            }
+
+            // Update the minimum number of swaps needed
+            minimumSwaps = Math.min(minimumSwaps, totalOnes - onesCount);
+        }
+
+        return minimumSwaps;
+    }
+}
