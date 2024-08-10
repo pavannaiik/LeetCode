d9a5209b41058639f5c463a2b12c43eb99ebaba6
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-08-10 14:22:43 -0400

    Time: 5 ms (38.31%), Space: 40.9 MB (44.35%) - LeetHub

---
+++ 1971-incremental-memory-leak/1971-incremental-memory-leak.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] memLeak(int memory1, int memory2) {
+        int[] ans = new int[3];
+        int i=1;
+        while( i <= memory1 || i <= memory2){
+            if(memory1 >= memory2 && i <= memory1 ){
+                memory1-=i;
+            }else{
+                if(i <= memory2)
+                    memory2-=i;
+            }
+            i++;
+        }
+        ans[0]=i;
+        ans[1]=memory1;
+        ans[2]=memory2;
+        return ans;
+    }
+}
