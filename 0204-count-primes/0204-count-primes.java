266597ed8fee54687f3cd346f807da0648a18887
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-08-18 16:35:58 -0400

    Time: 120 ms (23.56%), Space: 64.1 MB (17.38%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countPrimes(int n) {
+        int[] dp = new int[n];
+        int count =0;
+        for(int i=2;i<n;i++){
+            if(dp[i]!=-1){
+                count++;
+                for(int j=2;i*j<n;j++){
+                    dp[i*j]=-1;
+                }
+            }
+        }
+        return count;
+
+        
+    }
+}
