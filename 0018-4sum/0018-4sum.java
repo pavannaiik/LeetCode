fcc2367436f07a8d64d10682780928cf62fbd22c
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-09-15 19:37:36 -0400

    Time: 19 ms (43.76%), Space: 45.2 MB (26.67%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,47 +1,46 @@
 class Solution {
-    int len =0;
+    int len;
     public List<List<Integer>> fourSum(int[] nums, int target) {
         len = nums.length;
         Arrays.sort(nums);
-        return kSum(nums,target, 4,0);
+        return kSum(nums, target, 4, 0);
+        
     }
-    public ArrayList<List<Integer>> kSum(int[] nums,long target, int K, int index ){
-        ArrayList<List<Integer>>ans = new ArrayList<>();
+    public List<List<Integer>> kSum(int [] nums, long target, int k, int index){
+        List<List<Integer>>ans = new ArrayList<>();
         if(index >= len){
             return ans;
         }
-        if(K==2){
-            int i = index, j= len-1;
+        if(k==2){
+            int i= index, j = len-1;
             while(i < j){
-                long sum = (long) nums[i]+nums[j];
-                if(sum==target){
+                long sum = nums[i]+nums[j];
+                if(sum == target){
                     List<Integer>list = new ArrayList<>();
                     list.add(nums[i]);
                     list.add(nums[j]);
                     ans.add(list);
-                    while(i<j && nums[i]==nums[i+1]) i++;
-                    while(i<j && nums[j]==nums[j-1])j--;
+                    while( i < len-1 && nums[i]==nums[i+1]) i++;
+                    while(j >0 && nums[j]==nums[j-1])j--;
                     i++;
                     j--;
-                }else if(sum < target){
-                    i++;
+                }else if(sum>target){
+                    j--;
                 }else{
-                    j--;
+                    i++;
                 }
             }
-
         }else{
-            for(int i=index;i<nums.length-K+1;i++){
-                ArrayList<List<Integer>>res = kSum(nums, target-nums[i], K-1, i+1);
+            
+            for(int i=index;i<len-k+1;i++){
+                List<List<Integer>>res = kSum(nums, target-nums[i], k-1, i+1);
                 if(res!=null){
-                    for(List<Integer>temp:res){
-                        temp.add(0, nums[i]);
+                    for(List<Integer>list:res){
+                        list.add(0, nums[i]);
                     }
                     ans.addAll(res);
                 }
-                while(i<len-1 && nums[i]==nums[i+1]){
-                    i++;
-                }
+                while(i < len-1 && nums[i]==nums[i+1]) i++;
             }
         }
         return ans;
