41dc6e2f0ebf42f86390d7004ef104a39da71cf4
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-09-15 22:37:17 -0400

    Time: 14 ms (76.29%), Space: 69.2 MB (22.85%) - LeetHub

--- 2617-time-taken-to-cross-the-door/2617-time-taken-to-cross-the-door.java
+++ 2617-time-taken-to-cross-the-door/2617-time-taken-to-cross-the-door.java
@@ -1,43 +1,38 @@
 class Solution {
-    
     public int[] timeTaken(int[] arrival, int[] state) {
-        int n = arrival.length;
-        int[] answer = new int[n];
+        int len = arrival.length;
+        int[] ans = new int[len];
+        int lastDoorUsed =-1;
         Queue<Integer>enter = new LinkedList<>();
         Queue<Integer>exit = new LinkedList<>();
-        int lastDoorUsed =-1;
-        for(int i=0;i<n;i++){
-            if(state[i]==0){
-                enter.add(i);
-            }else{
-                exit.add(i);
-            }
+        for(int i=0;i<len;i++){
+            if(state[i]==0) enter.add(i);
+            else exit.add(i);
         }
         int time;
-        for(time =0;!enter.isEmpty()&& !exit.isEmpty();time++){
-            if(arrival[enter.peek()]<=time && (arrival[exit.peek()]>time || lastDoorUsed==0) ){
-                answer[enter.poll()] =time;
-                lastDoorUsed = 0;
+        for(time=0; !exit.isEmpty() && !enter.isEmpty() ; time++){
+            if(arrival[enter.peek()] <= time && (arrival[exit.peek()]>time || lastDoorUsed==0 )){
+                ans[enter.poll()]= time;
+                lastDoorUsed =0;
             }else if(arrival[exit.peek()] <= time){
-                answer[exit.poll()] = time;
+                ans[exit.poll()]=time;
                 lastDoorUsed = 1;
             }else{
-                lastDoorUsed=-1;
+                lastDoorUsed =-1;
             }
         }
-        
         while(!enter.isEmpty()){
-            if(arrival[enter.peek()]<=time){
-                answer[enter.poll()]=time;
+            if(arrival[enter.peek()] <= time){
+                ans[enter.poll()]= time;
             }
             time++;
         }
         while(!exit.isEmpty()){
-            if(arrival[exit.peek()]<=time){
-                answer[exit.poll()]=time;
+            if(arrival[exit.peek()] <= time){
+                ans[exit.poll()]= time;
             }
             time++;
         }
-        return answer;
+        return ans;
     }
 }
