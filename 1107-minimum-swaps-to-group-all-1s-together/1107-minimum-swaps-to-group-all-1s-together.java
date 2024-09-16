b44c80a233e1a41719daa3ea03b7f06b384a9dec
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-09-15 23:05:11 -0400

    Time: 2 ms (100%), Space: 59.1 MB (47.55%) - LeetHub

--- 1107-minimum-swaps-to-group-all-1s-together/1107-minimum-swaps-to-group-all-1s-together.java
+++ 1107-minimum-swaps-to-group-all-1s-together/1107-minimum-swaps-to-group-all-1s-together.java
@@ -1,21 +1,23 @@
 class Solution {
     public int minSwaps(int[] data) {
-        long ones =0;
-        ones = Arrays.stream(data).filter(num->num==1).count();
-        int left =0, right = 0;
-        int n = data.length;
-        int sum =0;
-        int maxSum =0;
-        while(right < n){
-            sum += data[right];
-            right++;
-            if(right-left > ones){
-                sum -= data[left];
-                left++;
-            }
-            maxSum = Math.max(maxSum, sum);
+        int count = 0;
+        for (int num : data) {
+            count += num;
         }
-        return (int)ones - maxSum;
+
+        int left = 0, sum = 0;
+        for (int right = 0; right < count; right++) {
+            sum += data[right];
+        }
+
+        int ans = count - sum;
+        for (int right = count; right < data.length; right++) {
+            sum += data[right];
+            sum -= data[left++];
+            ans = Math.min(ans, count - sum);
+        }
+
+        return ans;
 
     }
 }
