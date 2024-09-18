57b262ac47368ce3e2510a44c3cc26ab4989bba9
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-09-18 16:45:40 -0400

    Time: 25 ms (59.55%), Space: 45.3 MB (66.67%) - LeetHub

--- 2325-number-of-ways-to-select-buildings/2325-number-of-ways-to-select-buildings.java
+++ 2325-number-of-ways-to-select-buildings/2325-number-of-ways-to-select-buildings.java
@@ -2,36 +2,20 @@ class Solution {
     public long numberOfWays(String s) {
         long ans =0;
         int n = s.length();
-        long[] leftZeros = new long[n];
-        long[] rightZeros = new long[n];
-        long[] leftOnes = new long[n];
-        long[] rightOnes = new long[n];
-        long one=0, zero=0;
+        long totalZeros = 0;
+        long totalOnes =0;
+        for(char c:s.toCharArray()){
+            totalZeros += c=='0'?1:0;
+        }
+        totalOnes = n-totalZeros;
+        long curZeros =0, curOnes =0;
         for(int i=0;i<n;i++){
-            leftZeros[i] = zero;
-            leftOnes[i] = one;
             if(s.charAt(i)=='0'){
-                zero++;
+                ans += (curOnes * (totalOnes-curOnes));
+                curZeros++;
             }else{
-                one++;
-            }
-            
-        }
-        one=0; zero=0;
-         for(int i=n-1;i>=0;i--){
-             rightZeros[i] = zero;
-             rightOnes[i] = one;
-            if(s.charAt(i)=='0'){
-                zero++;
-            }else{
-                one++;
-            }
-        }
-        for(int i=1;i<n-1;i++){
-             if(s.charAt(i)=='0'){
-               ans += leftOnes[i]*rightOnes[i];
-            }else{
-                ans += leftZeros[i]*rightZeros[i];
+                ans += (curZeros * (totalZeros-curZeros));
+                curOnes++;
             }
         }
         return ans;
