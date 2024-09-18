b89089081e191ec0b341a1adc8785c01d6a645d5
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-09-18 16:04:06 -0400

    Time: 9 ms (49.73%), Space: 45.7 MB (12.09%) - LeetHub

--- 2405-minimum-number-of-keypresses/2405-minimum-number-of-keypresses.java
+++ 2405-minimum-number-of-keypresses/2405-minimum-number-of-keypresses.java
@@ -1,20 +1,14 @@
 class Solution {
     public int minimumKeypresses(String s) {
-        int[] arr= new int[26];
+        Integer[] arr= new Integer[26];
+        Arrays.fill(arr,0);
         for(char c:s.toCharArray()){
             arr[c-'a']++;
         }
-        Arrays.sort(arr);
-        int count =0;
-        int num=0;
-        for(int i=25;i>=0;i--){
-            if(arr[i] > 0){
-                if(num<9) count+=arr[i];
-                else if(num<18) count+= (arr[i]*2);
-                else if(num >14) count+=(arr[i]*3);
-                num++;
-            }
-        }
-        return count;
+        Arrays.sort(arr, (x,y)->y-x);
+        int result = 0;
+        for(int i = 0 ;i < 26; i++)
+            result += arr[i] * ((i + 9)/9);
+        return result;
     }
 }
