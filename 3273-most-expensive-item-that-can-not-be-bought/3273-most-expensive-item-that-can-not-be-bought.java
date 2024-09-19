bc23ac143d33800e0c4253a9b7885be9a308ec46
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-09-18 20:54:02 -0400

    Time: 994 ms (5.15%), Space: 40.9 MB (64.17%) - LeetHub

--- 3273-most-expensive-item-that-can-not-be-bought/3273-most-expensive-item-that-can-not-be-bought.java
+++ 3273-most-expensive-item-that-can-not-be-bought/3273-most-expensive-item-that-can-not-be-bought.java
@@ -1,5 +1,17 @@
 class Solution {
     public int mostExpensiveItem(int primeOne, int primeTwo) {
-        return primeOne*primeTwo-primeOne-primeTwo;
+        for(int i=primeOne*primeTwo;i>0;i--){
+            if(!canBeFormed(primeOne,primeTwo,i)){
+                return i;
+            }
+        }
+        return 1;
+    }
+    public boolean canBeFormed(int p1, int p2, int n){
+        for(int i=0;i<= n/p1;i++){
+            int remainder = n- p1*i;
+            if(remainder %p2==0) return true;
+        }
+        return false;
     }
 }
