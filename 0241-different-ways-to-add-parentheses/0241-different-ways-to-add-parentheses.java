5f9d8acdc314908485a1d80ce6e120ed34cb1fbe
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-09-22 14:37:01 -0400

    Time: 1 ms (99.73%), Space: 42 MB (73.87%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,22 +1,64 @@
 class Solution {
     public List<Integer> diffWaysToCompute(String expression) {
-        List<Integer>res = new ArrayList<>();
-        for(int i=0;i<expression.length();i++){
-            char operator = expression.charAt(i);
-            if(operator == '+' || operator == '-' || operator == '*' ){
-                List<Integer>leftList = diffWaysToCompute(expression.substring(0,i));
-                List<Integer>rightList = diffWaysToCompute(expression.substring(i+1));
-                for(int a:leftList){
-                    for(int b : rightList){
-                        if(operator == '+') res.add(a+b);
-                        else if(operator == '-') res.add(a-b);
-                        else if(operator == '*') res.add(a*b);
+        List<Integer>[][] memo = new ArrayList[expression.length()][expression.length()];
+        return computeResults(expression, memo, 0, expression.length()-1);
+    }
+    public List<Integer> computeResults(String expression,List<Integer>[][] memo, int start, int end ){
+        if(memo[start][end]!=null){
+            return memo[start][end];
+        }
+        List<Integer>results = new ArrayList<>();
+        //base case single digit
+        if(start==end){
+            results.add(expression.charAt(start)-'0');
+            return results;
+        }
+        // base case two digits
+        if(end-start==1 && Character.isDigit(expression.charAt(start))){
+            int tens = expression.charAt(start)-'0';
+            int ones = expression.charAt(end)-'0';
+            results.add(10*tens+ones);
+            return results;
+        }
+        for(int i=start;i<=end;i++){
+            char curChar = expression.charAt(i);
+            if(Character.isDigit(curChar)){
+                continue;
+            }
+            List<Integer> leftResults = computeResults(
+                expression,
+                memo,
+                start,
+                i - 1
+            );
+            List<Integer> rightResults = computeResults(
+                expression,
+                memo,
+                i + 1,
+                end
+            );
+            // Combine results from left and right subexpressions
+            for (int leftValue : leftResults) {
+                for (int rightValue : rightResults) {
+                    switch (curChar) {
+                        case '+':
+                            results.add(leftValue + rightValue);
+                            break;
+                        case '-':
+                            results.add(leftValue - rightValue);
+                            break;
+                        case '*':
+                            results.add(leftValue * rightValue);
+                            break;
                     }
                 }
             }
+
+            
         }
-        if(res.isEmpty()) res.add(Integer.parseInt(expression));
-        return res;
+        // Memoize the result for this subproblem
+        memo[start][end] = results;
+
+        return results;
     }
-   
 }
