d77840e10cadf6eea3a42a797f6cdaaa34103942
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-09-29 00:36:06 -0400

    Time: 9 ms (68.01%), Space: 45.2 MB (34.24%) - LeetHub

---
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String longestWord(String[] words) {
+         Trie trie = new Trie();
+        String longestValidWord = "";
+        for(String word: words){
+            trie.insert(word);
+        }
+        for(String word:words){
+            if(trie.hasAllPrefixes(word)){
+                if(word.length() > longestValidWord.length() || (word.length() == longestValidWord.length() && word.compareTo(longestValidWord) < 0)){
+                    longestValidWord = word;
+                }
+            }
+        }
+        return longestValidWord;
+
+    }
+    private static class Trie{
+        private static class TrieNode{
+            TrieNode[] children = new TrieNode[26];
+            boolean isEndOfWord;
+        }
+        private final TrieNode root = new TrieNode();
+        //insert
+        public void insert(String word) {
+            TrieNode node = root;
+            for( char c: word.toCharArray()){
+                int index = c-'a';
+                if(node.children[index]==null){
+                    node.children[index]=new TrieNode();
+                }
+                node = node.children[index];
+            }
+            node.isEndOfWord = true;
+        }
+        public boolean hasAllPrefixes( String word){
+            TrieNode node = root;
+            for (char c: word.toCharArray()){
+                node = node.children[c-'a'];
+                if(node == null || !node.isEndOfWord){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
