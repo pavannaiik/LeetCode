b714a9c64c888219c43aa39c4a661d83ed481228
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-10-10 19:10:10 -0400

    Time: 0 ms (100%), Space: 41.6 MB (28.04%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -1,23 +1,18 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        int left=0, right=0;
+        int left=0;
         int ans=0;
         for(char ch:s.toCharArray()){
             if(ch=='('){
                 left++;
             }else {
-                right++;
+                if(left >0){
+                    left--;
+                }else{
+                    ans++;
+                }
             }
-            if(left==right){
-                left=0;
-                right=0;
-            }else if(right > left){
-                ans+=right-left;
-                right = left;
-            }else if(left > right){
-                left = left-right;
-                right =0;
-            }
+            
         }
         return ans+left;
     }
