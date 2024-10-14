d47cba589d87ab509d0a8d29c6d4a5170cb8d89b
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-10-13 20:41:26 -0400

    Time: 326 ms (90.74%), Space: 59.3 MB (47.53%) - LeetHub

--- 2094-remove-stones-to-minimize-the-total/2094-remove-stones-to-minimize-the-total.java
+++ 2094-remove-stones-to-minimize-the-total/2094-remove-stones-to-minimize-the-total.java
@@ -1,19 +1,21 @@
 class Solution {
     public int minStoneSum(int[] piles, int k) {
-        int total =Arrays.stream(piles).sum();
-        PriorityQueue<Integer>pq = new PriorityQueue<>((a,b)-> (int) Math.floor((double)b/2)-(int)Math.floor((double) a/2));
-        for(int pile:piles){
-            pq.offer(pile);
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> b - a);
+        for (int num: piles) {
+            heap.add(num);
         }
-        int removed =0;
-        while(k-->0){
-            int cur = pq.poll();
-            int remaining = (int) Math.floor((double)cur/2);
-
-            removed += remaining;
-            pq.offer(cur - remaining);
+        
+        for (int i = 0; i < k; i++) {
+            int curr = heap.remove();
+            int remove = curr / 2;
+            heap.add(curr - remove);
         }
-        return total-removed;
-
+        
+        int ans = 0;
+        for (int num: heap) {
+            ans += num;
+        }
+        
+        return ans;
     }
 }
