d864348a6a1e066af90feac9e106f0dd973ea516
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-10-15 01:38:54 -0400

    Time: 7 ms (100%), Space: 45.4 MB (54.12%) - LeetHub

---
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long minimumSteps(String s) {
+        long totalSwaps=0;
+        int blackBallCount=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='0'){
+                totalSwaps += blackBallCount;
+            }else{
+                blackBallCount++;
+            }
+        }
+        return totalSwaps;
+       
+    }
+}
