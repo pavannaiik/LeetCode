c5e4e26520de6a2e4740de3c6afa8fa65010a2da
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-10-17 12:49:35 -0400

    Time: 0 ms (100%), Space: 40.5 MB (39.97%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -1,21 +1,21 @@
 class Solution {
     public int maximumSwap(int num) {
         char[] digits = String.valueOf(num).toCharArray();
-        int[] lastOccurance = new int[10];
-        for(int i=0;i<digits.length;i++){
-            lastOccurance[digits[i]-'0']=i;
-        }
-        for(int i=0;i<digits.length;i++){
-            for(int d=9;d>digits[i]-'0';d--){
-                if(lastOccurance[d] > i ){
-                    char temp = digits[i];
-                    digits[i] = digits[lastOccurance[d]];
-                    digits[lastOccurance[d]] = temp;
-                    // Return the result as an integer
-                    return Integer.parseInt(new String(digits));
-                }
+        int maxDigitIndex = -1 , swap1Idx=-1, swap2Idx=-1;
+        int n = digits.length;
+        for(int i=n-1;i>=0;i--){
+            if(maxDigitIndex ==-1 || digits[i] > digits[maxDigitIndex] ){
+                maxDigitIndex =i;
+            }else if( digits[i] < digits[maxDigitIndex]){
+                swap1Idx =i;
+                swap2Idx =maxDigitIndex;
             }
         }
-        return num;
+        if(swap1Idx !=-1 && swap2Idx !=-1){
+            char temp = digits[swap1Idx];
+            digits[swap1Idx]=digits[swap2Idx];
+            digits[swap2Idx]=temp;
+        }
+        return Integer.parseInt(new String(digits));
     }
 }
