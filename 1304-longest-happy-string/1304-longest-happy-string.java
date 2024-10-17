56cf08788dd312126176534bcc7f92e5d767aeaf
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-10-17 14:48:41 -0400

    Time: 0 ms (100%), Space: 40.9 MB (51.52%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.java
+++ 1304-longest-happy-string/1304-longest-happy-string.java
@@ -1,39 +1,44 @@
 class Solution {
+
     public String longestDiverseString(int a, int b, int c) {
-        PriorityQueue<Pair>pq=new PriorityQueue<>((x,y)->y.count-x.count);
-        if(a>0) pq.add(new Pair(a,'a'));
-        if(b>0) pq.add(new Pair(b,'b'));
-        if(c>0) pq.add(new Pair(c,'c'));
-        StringBuilder sb = new StringBuilder();
-        while(!pq.isEmpty()){
-            Pair p = pq.poll();
-            int count = p.count;
-            char ch=p.ch;
-            int n =sb.length();
-            if(n>=2 && sb.charAt(n-1)==ch && sb.charAt(n-2)==ch){
-                if (pq.isEmpty()) break;
-                Pair temp = pq.poll();
-                sb.append(temp.ch);
-                if(temp.count-1>0){
-                    pq.add(new Pair(temp.count-1, temp.ch));
-                }
-            }else{
-                count--;
-                sb.append(ch);
-            }
-            if(count > 0){
-                pq.add(new Pair(count, ch));
+        int curra = 0, currb = 0, currc = 0;
+        // Maximum total iterations possible is given by the sum of a, b and c.
+        int totalIterations = a + b + c;
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < totalIterations; i++) {
+            if (
+                (a >= b && a >= c && curra != 2) ||
+                (a > 0 && (currb == 2 || currc == 2))
+            ) {
+                // If 'a' is maximum and it's streak is less than 2, or if streak of 'b' or 'c' is 2, then 'a' will be the next character.
+                ans.append('a');
+                a--;
+                curra++;
+                currb = 0;
+                currc = 0;
+            } else if (
+                (b >= a && b >= c && currb != 2) ||
+                (b > 0 && (currc == 2 || curra == 2))
+            ) {
+                // If 'b' is maximum and it's streak is less than 2, or if streak of 'a' or 'c' is 2, then 'b' will be the next character.
+                ans.append('b');
+                b--;
+                currb++;
+                curra = 0;
+                currc = 0;
+            } else if (
+                (c >= a && c >= b && currc != 2) ||
+                (c > 0 && (curra == 2 || currb == 2))
+            ) {
+                // If 'c' is maximum and it's streak is less than 2, or if streak of 'a' or 'b' is 2, then 'c' will be the next character.
+                ans.append('c');
+                c--;
+                currc++;
+                curra = 0;
+                currb = 0;
             }
         }
-        return sb.toString();
-       
-    }
-}
-class Pair{
-    int count;
-    char ch;
-    Pair(int count, char ch){
-        this.count=count;
-        this.ch=ch;
+        return ans.toString();
     }
 }
