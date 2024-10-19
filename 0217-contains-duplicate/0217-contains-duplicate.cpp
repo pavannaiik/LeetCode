3417a388e6ef54e760bd888a522e1b80d904cb5a
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-10-19 19:01:10 -0400

    Time: 65 ms (97.46%), Space: 73.4 MB (63.48%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int>s;
+        for(int num:nums){
+            if(s.contains(num)){
+                return true;
+            }
+            s.insert(num);
+        }
+        return false;
+    }
+};
