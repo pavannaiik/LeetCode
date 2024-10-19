4cc8d30e576b8317e2e5ab221868a4edbae61e78
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-10-19 18:51:08 -0400

    Time: 20 ms (99.64%), Space: 25.3 MB (38%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        vector<vector<int>>dir ={{0,1},{1,0},{0,-1},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
-        if(grid[0][0]==1) return -1;
-        queue<vector<int>>queue;
-        queue.push({0,0,0});
-        int n =grid.size();
-        int m =grid[0].size();
-        while(!queue.empty()){
-            vector<int>a = queue.front();
-            queue.pop();
-            int r =a[0];
-            int c=a[1];
-            int dis=a[2];
-            if(r==n-1 && c==m-1){
-                return dis+1;
-            }
-            for(auto d:dir){
-                int nr = r+d[0];
-                int nc=c+d[1];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==0){
-                    queue.push({nr,nc,dis+1});
-                    grid[nr][nc]=-1;
+    int shortestPathBinaryMatrix(vector<vector<int>>& a) {
+        int n=a.size();
+        queue<pair<int,pair<int,int>>>q;
+        vector<vector<int>>vis(n,vector<int>(n,0));
+        if(a[0][0])return -1;
+        q.push({1,{0,0}});
+        vis[0][0]=1;
+        while(!q.empty()){
+            auto k=q.front();
+            q.pop();
+            int dist=k.first;
+            int r=k.second.first,c=k.second.second;
+            if(r==n-1 && c==n-1)return dist;
+            for(int i=-1;i<=1;i++){
+                for(int j=-1;j<=1;j++){
+                    int x=r+i,y=c+j;
+                    if(x>=0 && x<n && y>=0 && y<n && a[x][y]==0 && vis[x][y]==0){
+                        q.push({dist+1,{x,y}});
+                        vis[x][y]=1;
+                    }
                 }
             }
         }
