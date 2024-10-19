e8debb4b347129f9a0229f5855b11ec137685d91
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-10-19 17:19:00 -0400

    Time: 85 ms (7.43%), Space: 30.3 MB (13.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,9 +3,18 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>>ans;
         map<string,vector<string>>map;
+        int count[26];
         for(string s:strs){
-            string key= s;
-            sort(key.begin(), key.end());
+            fill(begin(count),end(count),0);
+            for(char c:s) count[c-'a']++;
+            string key="";
+            for(int i:count){
+                key+='#';
+                key+=to_string(i);
+            }
+            if(map.find(key)==map.end()){
+                map[key]=vector<string>();
+            }
             map[key].push_back(s);
         }
         for(auto& p:map){
