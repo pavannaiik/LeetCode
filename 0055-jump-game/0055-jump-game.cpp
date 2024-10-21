c849be59b315e5422c7c170365dddf58d63cdc9d
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-10-21 18:45:20 -0400

    Time: 0 ms (100%), Space: 51.2 MB (19.43%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int  n = nums.size();
-        int curFar=0,curEnd =0;
-        for(int i=0;i<n-1;i++){
-            curFar = max(curFar,i+nums[i]);
-                if(i==curEnd){
-                    curEnd=curFar;
-                }
+        //goal is to reach last index
+        int goal =nums.size()-1;
+        for(int i=nums.size()-2;i>=0;--i){
+            if(i+nums[i] >= goal){
+                goal=i;
+            }
         }
-        if(curEnd<n-1){
-            return false;
-        }
-        return true;
+        return goal>0?false:true;
     }
 };
