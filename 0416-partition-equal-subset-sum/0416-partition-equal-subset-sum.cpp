b74c99efe001271ae954df9c5263ffd949c3487e
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-10-21 17:05:00 -0400

    Time: 0 ms (100%), Space: 13.5 MB (85.44%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int sum=0;
-        for(int n:nums) sum+=n;
-        if(sum%2==1) return false;
-        int target=sum/2;
-        vector<bool> dp(target + 1, false);  // dp[i] means if we can get sum i from the subset
-        dp[0] = true;  // A subset sum of 0 is always possible (by taking no elements)
-        
-        // Iterate through each number in nums
-        for (int num : nums) {
-            // Update dp array from right to left (to avoid overwriting results prematurely)
-            for (int i = target; i >= num; i--) {
-                dp[i] = dp[i] || dp[i - num];
-            }
+       int n = nums.size();
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        if (sum & 1) return false;
+        sum >>= 1;
+
+        bitset<10001> dp(1);
+        for (int v: nums){
+            dp |= dp << v;
+            if (dp[sum]) return true;
         }
-        
-        // Check if it's possible to achieve the target sum
-        return dp[target];
+
+        return false;
 
     }
 };
