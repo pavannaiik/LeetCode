eed35ac0a0a98fc5962fa9d0353ebf1630b26ee7
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-10-21 01:49:39 -0400

    Time: 1 ms (99.76%), Space: 17.1 MB (86.18%) - LeetHub

---
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<pair<int,int>>dir = {{0,1},{0,-1},{1,0},{-1,0}};
+
+    void wallsAndGates(vector<vector<int>>& rooms) {
+        int n= rooms.size(),m=rooms[0].size();
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(rooms[i][j]==0){
+                    q.push({i,j});
+                }
+            }
+        }
+        while(!q.empty()){
+            pair p = q.front();
+            q.pop();
+            int r = p.first;
+            int c= p.second;
+            for(int i=0;i<4;i++){
+                int R=r+dir[i].first;
+                int C=c+dir[i].second;
+                if(R<0 || R>=n || C <0 || C>= m || rooms[R][C] != INT_MAX){
+                    continue;;
+                }
+                rooms[R][C]=1+rooms[r][c];
+                q.push({R,C});
+            }
+
+        }
+    }
+};
