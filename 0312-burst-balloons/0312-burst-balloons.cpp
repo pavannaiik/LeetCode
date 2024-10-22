a8e9800274e9d149839dc41101660e08a1129a85
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-10-22 01:10:40 -0400

    Time: 215 ms (14.93%), Space: 12.4 MB (33.63%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     int maxCoins(vector<int>& nums) {
-        int n = nums.size() + 2;  // Size with padding
-        vector<vector<int>> dp(n, vector<int>(n, 0));  // DP table initialization
-        vector<int> new_nums(n, 1);  // Padded nums array with 1s at the boundaries
-
-        // Filling new_nums with original nums
-        for (int i = 1; i < n - 1; ++i) {
-            new_nums[i] = nums[i - 1];
+        int n=nums.size();
+        vector<int>newNums(n+2,1);
+        for(int i=1;i<=n;i++){
+            newNums[i]=nums[i-1];
         }
-
-        // Dynamic programming to calculate maxCoins
-        for (int len = 2; len < n; ++len) {  // len is the subarray length
-            for (int i = 0; i < n - len; ++i) {
-                int j = i + len;  // Right boundary of subarray
-                for (int k = i + 1; k < j; ++k) {  // Position to burst the last balloon
-                    dp[i][j] = max(dp[i][j], dp[i][k] + dp[k][j] + new_nums[i] * new_nums[k] * new_nums[j]);
-                }
-            }
+        vector<vector<int>>memo(n+1,vector<int>(n+1,-1));;
+        int coins= calculateMaxCoins(1,n,newNums,memo);
+        return coins;
+    }
+    int calculateMaxCoins(int i, int j, vector<int>&newNums,  vector<vector<int>>&memo){
+        if(i>j){
+            return 0;
         }
-
-        return dp[0][n - 1];  // The answer is the maxCoins for the entire range [0, n-1]
+        if(memo[i][j]!=-1){
+            return memo[i][j];
+        }
+        int maxcoins=INT_MIN;
+        for(int l=i;l<=j;l++){
+            int c=newNums[i-1]*newNums[l]*newNums[j+1]+calculateMaxCoins(i,l-1,newNums,memo)+calculateMaxCoins(l+1,j,newNums,memo);
+            maxcoins=max(maxcoins,c);
+        }
+        return memo[i][j]=maxcoins;
     }
 };
