90afd01713fa9be91d2e3cc76241007f0e8b4263
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-10-22 01:13:07 -0400

    Time: 207 ms (34.99%), Space: 12.4 MB (7.1%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     int maxCoins(vector<int>& nums) {
         int n=nums.size();
-        vector<int>newNums(n+2,1);
-        for(int i=1;i<=n;i++){
-            newNums[i]=nums[i-1];
-        }
+        nums.push_back(1);
+        nums.insert(nums.begin(), 1);
         vector<vector<int>>memo(n+1,vector<int>(n+1,-1));;
-        int coins= calculateMaxCoins(1,n,newNums,memo);
+        int coins= calculateMaxCoins(1,n,nums,memo);
         return coins;
     }
     int calculateMaxCoins(int i, int j, vector<int>&newNums,  vector<vector<int>>&memo){
