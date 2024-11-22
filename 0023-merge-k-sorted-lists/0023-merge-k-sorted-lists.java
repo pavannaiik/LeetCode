8d99c905e4004d59b09a5f15e9c4a937e8f944b6
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-11-22 13:19:40 -0500

    Time: 1 ms (99.87%), Space: 44.3 MB (67.96%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -25,15 +25,25 @@ class Solution {
 
     }
     public ListNode merge(ListNode l1, ListNode l2){
-        if(l1==null) return l2;
-        if(l2==null) return l1;
-        if(l1.val < l2.val){
-            l1.next=merge(l1.next, l2);
-            return l1;
-        }else{
-            l2.next = merge(l1, l2.next);
-            return l2;
+        ListNode head = new ListNode(0);
+        ListNode point = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                point.next = l1;
+                l1 = l1.next;
+            } else {
+                point.next = l2;
+                l2 = l1;
+                l1 = point.next.next;
+            }
+            point = point.next;
         }
+        if (l1 == null) {
+            point.next = l2;
+        } else {
+            point.next = l1;
+        }
+        return head.next;
     }
     
 }
