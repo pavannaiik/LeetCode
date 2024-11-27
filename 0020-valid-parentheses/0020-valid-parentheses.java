ecada612e374f90d0dc336db04da76d0fa8abb76
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-11-27 15:35:05 -0500

    Time: 2 ms (97.4%), Space: 41.9 MB (15.15%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
         if(s.length()==1) return false;
-        Stack<Character>stack = new Stack<>();
-        for(char ch : s.toCharArray()){
-            if(ch == '(' ){
-                stack.push(')');
-            }else if(ch == '{'){
-                stack.push('}');
-            }else if(ch == '['){
-                stack.push(']');
-            }else{
+        Stack<Character>stack= new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='(') stack.push(')');
+            else if(ch=='{') stack.push('}');
+            else if(ch=='[') stack.push(']');
+            else{
                 if(stack.size()==0 || stack.peek()!=ch){
                     return false;
-                }else{
-                    stack.pop();
                 }
+                stack.pop();
             }
         }
-        if(stack.size()!=0) return false;
-        return true;
+        return stack.size()==0;
     }
 }
