fdadfd88b27794a2329f98d3b9e6d3340c9ffad4
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-11-27 16:05:05 -0500

    Time: 12 ms (95.5%), Space: 42.3 MB (74.31%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,30 +1,27 @@
 class Solution {
-     int leftIndex =0, rightIndex =0;
-     int maxLen = Integer.MIN_VALUE;
-    public void findLP(String s, int p, int q){
-        int n =s.length();
-        while(p >=0 && q < n){
-            if(s.charAt(p)!=s.charAt(q)){
-                break;
-            }
-            p--;
-            q++;
-        }
-
-        int len = q-p-1;
-        if(len > maxLen){
-            maxLen = len;
-            leftIndex = p+1;
-            rightIndex = q-1;
-        }
-    }
+    int maxLen = Integer.MIN_VALUE;
+    int leftIndex=0, rightIndex =0;
     public String longestPalindrome(String s) {
-        int n =s.length();
-        for(int i=0;i<n;i++){
-            findLP(s,i,i);
+        for(int i=0;i<s.length();i++){
+            findLP(s, i, i);
             findLP(s,i,i+1);
         }
-        
-        return s.substring(leftIndex,rightIndex+1);
+        return s.substring(leftIndex, rightIndex+1);
+    }
+    public void findLP(String s, int left, int right){
+        int n = s.length();
+        while(left >=0 && right <n){
+            if(s.charAt(left)!=s.charAt(right)){
+                break;
+            }
+            left--;
+            right++;
+        }
+        int curLen = right-left-1;
+        if(curLen > maxLen){
+            maxLen = curLen;
+            leftIndex = left+1;
+            rightIndex = right-1;
+        }
     }
 }
