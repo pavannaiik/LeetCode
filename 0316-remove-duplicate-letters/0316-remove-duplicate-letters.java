632bf0df30b21827f812886edb1ac7206bc49011
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-11-28 01:49:02 -0500

    Time: 3 ms (52.67%), Space: 42.3 MB (45.37%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,35 +1,25 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-        if (s == null || s.length() == 0) {
-            return "";
-        }
-        
-        int[] count = new int[26]; // Frequency count of each character
-        for (char ch : s.toCharArray()) {
-            count[ch - 'a']++;
-        }
-
-        boolean[] seen = new boolean[26]; 
-        Stack<Character> stack = new Stack<>();
-
-        for (char ch : s.toCharArray()) {
-            count[ch - 'a']--; 
-
-            if (seen[ch - 'a']) {
-                continue; 
+        int[] count= new int[26];
+        for(char ch:s.toCharArray()) count[ch-'a']++;
+        boolean[] seen= new boolean[26];
+        Stack<Character>stack = new Stack<>();
+        for(char ch:s.toCharArray()){
+            count[ch-'a']--;
+            if(seen[ch-'a'] ){
+                continue;
             }
-            while (!stack.isEmpty() && stack.peek() > ch && count[stack.peek() - 'a'] > 0) {
-                seen[stack.pop() - 'a'] = false; 
+            while(!stack.isEmpty() && stack.peek()-'a' > ch-'a' && count[stack.peek()-'a'] >0){
+                seen[stack.peek()-'a']=false;
+                stack.pop();
             }
-
-            stack.push(ch);
-            seen[ch - 'a'] = true; 
+            stack.add(ch);
+            seen[ch-'a']=true;
         }
         StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
+        while(!stack.isEmpty()){
             sb.append(stack.pop());
         }
-
         return sb.reverse().toString();
     }
 }
