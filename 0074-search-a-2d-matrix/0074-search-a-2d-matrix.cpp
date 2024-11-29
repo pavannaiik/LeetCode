18423ddedac7562f1967ce6e7ed5cebde01be0bb
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-11-28 19:58:35 -0500

    Time: 0 ms (100%), Space: 13.2 MB (5.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -6,7 +6,7 @@ public:
         while(i<=j){
             int mid=i+(j-i)/2;
             int r=mid/m;
-            int c=mid%m;;
+            int c=mid%m;
             if(matrix[r][c]==target) return true;
             else if(matrix[r][c]>target) j=mid-1;
             else i=mid+1;
