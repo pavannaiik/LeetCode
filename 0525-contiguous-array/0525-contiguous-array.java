806793fed4152aefbe0b89e5a129e017ac8c66c7
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-11-29 01:19:16 -0500

    Time: 26 ms (11.27%), Space: 55.2 MB (41.74%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -5,10 +5,7 @@ class Solution {
         map.put(0,-1);
         int sum =0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                nums[i]=-1;
-            }
-            sum+=nums[i];
+            sum+=nums[i] == 1 ? 1 : -1;
             if(map.containsKey(sum)){
                 max=Math.max(max,i-map.get(sum));
             }else map.put(sum, i);
