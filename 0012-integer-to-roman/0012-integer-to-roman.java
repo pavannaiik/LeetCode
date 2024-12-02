4c082675e5f329edcabbdf1ea9f37c05efb5a441
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-12-01 22:20:42 -0500

    Time: 2 ms (100%), Space: 44.1 MB (76.72%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,48 +1,61 @@
 class Solution {
-    private static final String[] thousands = { "", "M", "MM", "MMM" };
-    private static final String[] hundreds = {
-        "",
-        "C",
-        "CC",
-        "CCC",
-        "CD",
-        "D",
-        "DC",
-        "DCC",
-        "DCCC",
-        "CM",
-    };
-    private static final String[] tens = {
-        "",
-        "X",
-        "XX",
-        "XXX",
-        "XL",
-        "L",
-        "LX",
-        "LXX",
-        "LXXX",
-        "XC",
-    };
-    private static final String[] ones = {
-        "",
-        "I",
-        "II",
-        "III",
-        "IV",
-        "V",
-        "VI",
-        "VII",
-        "VIII",
-        "IX",
-    };
-
-    public String intToRoman(int num) {
-        return (
-            thousands[num / 1000] +
-            hundreds[(num % 1000) / 100] +
-            tens[(num % 100) / 10] +
-            ones[num % 10]
-        );
+    public String intToRoman(int N) {
+        StringBuilder res = new StringBuilder();
+        while(N > 999){
+            res.append('M');
+            N -= 1000;
+        }
+        // --
+        if(N > 899){
+            res.append("CM");
+            N -= 900;
+        }
+        else if(N > 499){
+            res.append('D');
+            N -= 500;
+        }
+        else if(N > 399){
+            res.append("CD");
+            N -= 400;
+        }
+        while(N > 99){
+            res.append('C');
+            N -= 100;
+        }
+        // --
+        if(N > 89){
+            res.append("XC");
+            N -= 90;
+        }
+        else if(N > 49){
+            res.append('L');
+            N -= 50;
+        }
+        else if(N > 39){
+            res.append("XL");
+            N -= 40;
+        }
+        while(N > 9){
+            res.append('X');
+            N -= 10;
+        }
+        // --
+        if(N > 8){
+            res.append("IX");
+            N -= 9;
+        }
+        else if(N > 4){
+            res.append('V');
+            N -= 5;
+        }
+        else if(N > 3){
+            res.append("IV");
+            N -= 4;
+        }
+        while(N > 0){
+            res.append('I');
+            N -= 1;
+        }
+        return res.toString();
     }
 }
