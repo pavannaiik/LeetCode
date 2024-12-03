25c55cbb0c62aa4f68e8763e47f6493eb6b4fffb
pavannaiik <97593632+pavannaiik@users.noreply.github.com>
2024-12-02 20:37:02 -0500

    Time: 5 ms (98.54%), Space: 44.4 MB (9.74%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,18 +1,10 @@
 class Solution {
-    private class LargerNumberComparator implements Comparator<String>{
-        @Override
-        public int compare(String a, String b){
-            String order1 = a+b;
-            String order2 = b+a;
-            return order2.compareTo(order1);
-        }
-    }
     public String largestNumber(int[] nums) {
         String[] asString = new String[nums.length];
         for(int i=0;i<nums.length;i++){
             asString[i]=String.valueOf(nums[i]);
         }
-        Arrays.sort(asString, new LargerNumberComparator());
+        Arrays.sort(asString,(a, b) -> (b + a).compareTo(a + b));
         //after sorting if the largest number is zero then all zeros
         if(asString[0].equals("0")) return "0";
         String ans = new String();
