10f978291cac2d7862f333122773c87d29534646
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-05 03:49:10 -0500

    Time: 2 ms (97.61%), Space: 42 MB (19.76%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,19 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length()==1) return false;
-        Stack<Character>stack= new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch=='(') stack.push(')');
-            else if(ch=='{') stack.push('}');
-            else if(ch=='[') stack.push(']');
-            else{
-                if(stack.size()==0 || stack.peek()!=ch){
+        Stack<Character>stack=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='('){
+                stack.add(')');
+            }else if(ch=='{'){
+                stack.add('}');
+            }else if(ch=='['){
+                stack.add(']');
+            }else{
+                if(stack.isEmpty()){
                     return false;
                 }
-                stack.pop();
+                if(stack.peek()==ch){
+                    stack.pop();
+                }else{
+                    return false;
+                }
             }
         }
-        return stack.size()==0;
+        return stack.isEmpty();
     }
 }
