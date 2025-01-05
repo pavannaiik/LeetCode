fb436906a63398434b956c0675bb21a689ac7d0d
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-04 19:00:52 -0500

    Time: 68 ms (80.7%), Space: 57.5 MB (5.18%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Map<String, List<String>> map = new HashMap<>();
+        wordList.forEach(word -> {
+            for (int i = 0; i < word.length(); i++) {
+                String newWord = word.substring(0, i) + '*' + word.substring(i + 1);
+                map.computeIfAbsent(newWord, k -> new ArrayList<>()).add(word);
+            }
+        });
+
+        Queue<Pair<String, Integer>> queue = new LinkedList<>();
+        queue.add(new Pair<>(beginWord, 1));
+        Set<String> visited = new HashSet<>();
+        visited.add(beginWord);
+
+        while (!queue.isEmpty()) {
+            Pair<String, Integer> node = queue.poll();
+            String word = node.getKey();
+            int level = node.getValue();
+
+            for (int i = 0; i < word.length(); i++) {
+                String newWord = word.substring(0, i) + '*' + word.substring(i + 1);
+                for (String adjacentWord : map.getOrDefault(newWord, new ArrayList<>())) {
+                    if (adjacentWord.equals(endWord)) return level + 1;
+                    if (visited.add(adjacentWord)) queue.add(new Pair<>(adjacentWord, level + 1));
+                }
+            }
+        }
+        return 0;
+    }
+}
