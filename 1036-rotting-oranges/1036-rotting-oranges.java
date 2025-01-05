68acee260cca2aa2e43bb7f28242eaf60e196b5a
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-05 03:07:38 -0500

    Time: 1 ms (100%), Space: 42.1 MB (92.17%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,30 +1,36 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int ans = 2;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
+        int ans =2;
+        int n = grid.length, m = grid[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    dfs(grid, i, j, 2);
+                    dfs(grid, i, j, n, m, 2);
                 }
             }
         }
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1) return -1;
-                 ans = Math.max(ans,grid[i][j]);
+       // System.out.println(grid);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+                ans = Math.max(ans, grid[i][j]);
             }
         }
         return ans-2;
+
     }
-    public void dfs(int[][] grid, int i, int j, int time){
-        if(i <0 || j<0 || i>=grid.length || j>=grid[0].length||grid[i][j] == 0){
+    public void dfs(int[][] grid, int i, int j, int n, int m, int time){
+        if(i<0 || i>=n || j<0 ||j >=m|| grid[i][j]==0){
             return;
         }
         if(grid[i][j] > 1 && grid[i][j] < time) return;
-        grid[i][j]=time;
-        dfs(grid, i, j+1, time+1);
-        dfs(grid, i, j-1, time+1);
-        dfs(grid, i+1, j, time+1);
-        dfs(grid, i-1, j, time+1);
+        grid[i][j] = time;
+
+        dfs(grid, i, j+1, n, m, time+1);
+        dfs(grid, i, j-1, n, m, time+1);
+        dfs(grid, i+1, j, n, m, time+1);
+        dfs(grid, i-1, j, n, m, time+1);
     }
 }
