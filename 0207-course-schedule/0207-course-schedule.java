e2a07c60dbb33979f7b69e310038789ed6e85b58
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-05 21:27:02 -0500

    Time: 7 ms (51.34%), Space: 44.9 MB (85.11%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] inDegree = new int[numCourses];
+        List<List<Integer>>adjList = new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adjList.add(new ArrayList<>());
+        }
+        for(int[] arr: prerequisites){
+            adjList.get(arr[1]).add(arr[0]);
+            inDegree[arr[0]]++;
+        }
+        Queue<Integer>queue = new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(inDegree[i]==0){
+                queue.add(i);
+            }
+        }
+        List<Integer>ans= new ArrayList<>();
+        while(!queue.isEmpty()){
+            int curElement = queue.poll();
+            List<Integer>curList = adjList.get(curElement);
+            for(int i=0;i<curList.size();i++){
+                inDegree[curList.get(i)]--;
+                if(inDegree[curList.get(i)]==0){
+                    queue.add(curList.get(i));
+                }
+            }
+            ans.add(curElement);
+        }
+        if(ans.size()==numCourses)
+        return true;
+        else return false;
+    }
+}
