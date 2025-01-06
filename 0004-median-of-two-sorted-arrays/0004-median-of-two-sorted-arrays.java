f4bcc0a0fd5da99e04dd921fd43f4266386328b5
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-06 17:13:40 -0500

    Time: 1 ms (100%), Space: 46 MB (75.48%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,26 +1,27 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-       if(nums2.length<nums1.length){
-            return findMedianSortedArrays(nums2,nums1);
+        if(nums1.length>nums2.length){
+            return findMedianSortedArrays(nums2, nums1);
         }
-        int start=0,end=nums1.length;
-        while(start<=end){
-            int parX=(start+end)/2;
-            int parY=(nums1.length+nums2.length+1)/2-parX;
-            int maxX=(parX==0)?Integer.MIN_VALUE:nums1[parX-1];
-            int minX=(parX==nums1.length)?Integer.MAX_VALUE:nums1[parX];
-            int maxY=(parY==0)?Integer.MIN_VALUE:nums2[parY-1];
-            int minY=(parY==nums2.length)?Integer.MAX_VALUE:nums2[parY];
-            if(maxX<=minY && maxY<=minX){
-                if((nums1.length+nums2.length)%2==0){
-                    return (Math.max(maxX,maxY)+Math.min(minX,minY))/2.0;
+        int n=nums1.length, m = nums2.length;
+        int i=0, j=n;
+        while(i<=j){
+            int partX = i+(j-i)/2;
+            int partY = (m+n+1)/2-partX;
+            int maxX = partX==0?Integer.MIN_VALUE:nums1[partX-1];
+            int minX = partX==n?Integer.MAX_VALUE:nums1[partX];
+            int maxY = partY==0?Integer.MIN_VALUE:nums2[partY-1];
+            int minY = partY==m?Integer.MAX_VALUE:nums2[partY];
+            if(maxX <= minY && maxY <= minX){
+                if((n+m)%2==0){
+                    return (double) (Math.max(maxX,maxY)+Math.min(minX,minY))/2.0;
                 }else{
-                    return (double)(Math.max(maxX,maxY));
+                    return (double) Math.max(maxX,maxY);
                 }
-            }else if(maxX>minY){
-                end=parX-1;
+            }else if(minX > maxY){
+                j = partX-1;
             }else{
-                start=parX+1;
+                i= partX+1;
             }
         }
         return -1;
