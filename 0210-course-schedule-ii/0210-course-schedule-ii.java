f223a16b5abd005af1465d151214e0901950731c
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-05 20:31:59 -0500

    Time: 7 ms (37.47%), Space: 46.2 MB (32.88%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] inDegree = new int[numCourses];
+        List<List<Integer>>adjList = new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adjList.add(new ArrayList<>());
+        }
+        for(int[] arr: prerequisites){
+            adjList.get(arr[1]).add(arr[0]);
+            inDegree[arr[0]]++;
+        }
+        Queue<Integer>queue = new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(inDegree[i]==0){
+                queue.add(i);
+            }
+        }
+        if(queue.size()==0){
+            return new int[0];
+        }
+        List<Integer>ans= new ArrayList<>();
+        while(!queue.isEmpty()){
+            int curElement = queue.poll();
+            List<Integer>curList = adjList.get(curElement);
+            for(int i=0;i<curList.size();i++){
+                inDegree[curList.get(i)]--;
+                if(inDegree[curList.get(i)]==0){
+                    queue.add(curList.get(i));
+                }
+            }
+            ans.add(curElement);
+        }
+        if(ans.size()==numCourses)
+        return ans.stream().mapToInt(Integer::intValue).toArray();
+        else return new int[0];
+    }
+}
