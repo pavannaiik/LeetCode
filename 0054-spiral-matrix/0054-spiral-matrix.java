e9e234c93e7fb78dd86368a34c402f5af15148ff
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-06 15:32:28 -0500

    Time: 0 ms (100%), Space: 41.6 MB (35.18%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,43 +1,35 @@
 class Solution {
-        List<Integer>result = new ArrayList<>();
-    int ans=0;
     public List<Integer> spiralOrder(int[][] matrix) {
-        int cols= matrix[0].length;
-        int rows = matrix.length;
-        int up =0;
-        int down = rows-1;
-        int left =0;
-        int right = cols-1;
-        int direction =0;
-        //0 - move right column  wise
-        //1 move down row wise
-        //2 move left column wise
-        //3 move up row wise 
-        while(result.size()<cols*rows){
-            if(direction ==0){
+        int dir=0;
+        List<Integer>ans=new ArrayList<>();
+        int up=0, down =matrix.length-1;
+        int left=0, right = matrix[0].length-1;
+        int n = matrix.length, m = matrix[0].length;
+
+        while(ans.size()!= n*m ){
+            if(dir==0){
                 for(int i=left;i<=right;i++){
-                    result.add(matrix[up][i]);
+                    ans.add(matrix[up][i]);
                 }
                 up++;
-            }else if(direction ==1){
+            }else if(dir==1){
                 for(int i=up;i<=down;i++){
-                    result.add(matrix[i][right]);
+                    ans.add(matrix[i][right]);
                 }
                 right--;
-            }else if(direction ==2){
+            }else if(dir==2){
                 for(int i=right;i>=left;i--){
-                    result.add(matrix[down][i]);
+                    ans.add(matrix[down][i]);
                 }
                 down--;
-            }else if(direction ==3){
+            }else if(dir==3){
                 for(int i=down;i>=up;i--){
-                    result.add(matrix[i][left]);
+                    ans.add(matrix[i][left]);
                 }
                 left++;
             }
-            direction = ((direction+1)%4);
-            
+            dir= (dir+1)%4;
         }
-        return result;
+        return ans;
     }
 }
