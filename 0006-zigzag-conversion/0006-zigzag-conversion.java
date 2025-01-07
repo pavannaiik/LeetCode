9c12cd165e3d4f603c2715c77525562f882621b5
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-06 19:55:17 -0500

    Time: 3 ms (90.52%), Space: 45.3 MB (45.17%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,22 +1,22 @@
 class Solution {
     public String convert(String s, int numRows) {
         if(numRows==1) return s;
-        StringBuilder res = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         int charsInSection = 2*(numRows-1);
         for(int i=0;i<numRows;i++){
-            int index = i;
+            int index =i;
             while(index<s.length()){
-                res.append(s.charAt(index));
-                if(i !=0 && i != numRows-1){
-                    int charsInBetween = charsInSection- 2*i;
-                    int secondIndex = index + charsInBetween;
-                    if(secondIndex < s.length()){
-                        res.append(s.charAt(secondIndex));
+                sb.append(s.charAt(index));
+                if(i!=0 && i != numRows-1){
+                    int charsInBetween = charsInSection-2*i;
+                    int secondCharIndex = index + charsInBetween;
+                    if(secondCharIndex < s.length()){
+                        sb.append(s.charAt(secondCharIndex));
                     }
                 }
-                index+=charsInSection;
+                index += charsInSection;
             }
         }
-        return res.toString();
+        return sb.toString();
     }
 }
