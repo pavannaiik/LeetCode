153e010332aa88aab002400546c8bafdd958d091
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-08 01:17:31 -0500

    Time: 0 ms (100%), Space: 42 MB (81.2%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,6 +1,5 @@
-import java.util.Map;
 class Solution {
-    private List<String>result = new ArrayList<>();
+     private List<String>result = new ArrayList<>();
     private final Map<Character,String>map = Map.of('2',"abc",'3',"def",'4',"ghi",'5',"jkl",'6',"mno",'7',"pqrs",'8',"tuv",'9',"wxyz");
     public List<String> letterCombinations(String digits) {
         if(digits.length()==0) return result;
@@ -8,12 +7,12 @@ class Solution {
         return result;
     }
     public void backtrack(String digits, int index, StringBuilder sb){
-        if(sb.length() == digits.length()){
+        if( index==digits.length()){
             result.add(sb.toString());
             return;
         }
-        String temp = map.get(digits.charAt(index));
-        for(char ch: temp.toCharArray()){
+        String str = map.get(digits.charAt(index));
+        for(char ch:str.toCharArray()){
             sb.append(ch);
             backtrack(digits, index+1, sb);
             sb.deleteCharAt(sb.length()-1);
