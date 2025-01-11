e8e8a309d32614669f71fe25dabcdf9a8591c368
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-10 21:36:48 -0500

    Time: 27 ms (90.07%), Space: 59.4 MB (57.81%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,22 +1,20 @@
-
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b)->(a[0]-b[0]));
-        List<List<Integer>>ans = new ArrayList<>();
-        if(nums1.length ==0 || nums2.length==0 || k <=0) return ans;
-        for(int i=0;i<Math.min(k, nums1.length);i++){
-            minHeap.add(new int[]{nums1[i]+nums2[0], i,0});
+        PriorityQueue<int[]> minHeap=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        List<List<Integer>>result=new ArrayList<>();
+        if(nums1.length==0 || nums2.length==0 || k<=0) return result;
+        for(int i=0;i<Math.min(k,nums1.length);i++){
+            minHeap.add(new int[]{nums1[i]+nums2[0], i, 0});
         }
         while( k-- > 0 && !minHeap.isEmpty()){
-            int[] current = minHeap.poll();
-            int i= current[1];
+            int[] current=minHeap.poll();
+            int i = current[1];
             int j= current[2];
-            ans.add(Arrays.asList(nums1[i], nums2[j]));
+            result.add(List.of(nums1[i], nums2[j]));
             if(j+1 < nums2.length){
-                minHeap.offer(new int[]{nums1[i]+nums2[j+1], i, j+1});
+                minHeap.add(new int[]{nums1[i]+nums2[j+1], i, j+1});
             }
         }
-        return ans;
-
+        return result;
     }
 }
