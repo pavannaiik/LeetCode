6a19c53affe8a4972319e235f594e35e59f837cc
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-10 20:53:14 -0500

    Time: 9 ms (59.13%), Space: 48.1 MB (39.47%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.java
+++ 0286-walls-and-gates/0286-walls-and-gates.java
@@ -1,32 +1,33 @@
 class Solution {
     public void wallsAndGates(int[][] rooms) {
-        Queue<Pair<Integer,Integer>>queue = new LinkedList<>();
+        Queue<int[]> queue = new LinkedList<>();
         int n = rooms.length, m = rooms[0].length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(rooms[i][j]==0){
-                    queue.add(new Pair(i,j));
+
+        // Add all gates to the queue
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (rooms[i][j] == 0) {
+                    queue.add(new int[]{i, j});
                 }
             }
-        }
-        if(queue.size()==0) return;
-        int dist=1;
-        int[][] dir={{0,1},{1,0},{-1,0},{0,-1}};
-        while(!queue.isEmpty()){
-            int len = queue.size();
-            for(int j=0;j<len;j++){
-                Pair cell = queue.poll();
-                for(int i=0;i<4;i++){
-                int r = (int)cell.getKey() + dir[i][0];
-                int c = (int)cell.getValue() + dir[i][1];
-                if(r >=0 && r < n && c >=0 && c< m && (rooms[r][c]!=-1 && rooms[r][c] > dist)){
-                    queue.add(new Pair(r,c));
-                    rooms[r][c]=dist;
-                }
-            }
-            }
-            dist++;
         }
 
+        // Directions for moving up, down, left, right
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        // BFS traversal
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            for (int[] dir : directions) {
+                int r = cell[0] + dir[0];
+                int c = cell[1] + dir[1];
+
+                // Check bounds and update distance if it's an empty room
+                if (r >= 0 && r < n && c >= 0 && c < m && rooms[r][c] == Integer.MAX_VALUE) {
+                    rooms[r][c] = rooms[cell[0]][cell[1]] + 1;
+                    queue.add(new int[]{r, c});
+                }
+            }
+        }
     }
 }
