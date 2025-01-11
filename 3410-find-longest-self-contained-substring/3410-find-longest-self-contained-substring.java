8438af3f3e1ef79ffa206a8fb6027e529dab71bd
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-11 01:47:00 -0500

    Time: 7 ms (100%), Space: 45.2 MB (77.46%) - LeetHub

--- 3410-find-longest-self-contained-substring/3410-find-longest-self-contained-substring.java
+++ 3410-find-longest-self-contained-substring/3410-find-longest-self-contained-substring.java
@@ -1,44 +1,32 @@
 class Solution {
     public int maxSubstringLength(String s) {
-        if (s.isBlank()) return -1;
-        
         int n = s.length();
-        int[] first = new int[26];
+        int[] first=new int[26];
         int[] last = new int[26];
-        Arrays.fill(first, -1);
-
-        // Step 1: Calculate first and last occurrences of each character
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            if (first[c - 'a'] == -1) {
-                first[c - 'a'] = i;
+        Arrays.fill(first,-1 );
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            if(first[ch-'a']==-1){
+                first[ch-'a']=i;
             }
-            last[c - 'a'] = i;
+            last[ch-'a']=i;
         }
-
-        int res = -1;
-
-        // Step 2: Check for self-contained substrings
-        for (int i = 0; i < 26; i++) {
-            if (first[i] == -1) continue;
-
+        int res =-1;
+        for(int i=0;i<26;i++){
+            if(first[i]==-1) continue;
             int start = first[i];
             int end = last[i];
-
-            // Extend the substring to include all characters within the range [start, end]
-            for (int j = start; j < n; j++) {
-                char c = s.charAt(j);
-                int charStart = first[c-'a'];
-                int charEnd = last[c-'a'];
-                if(charStart < start) break;
-                end = Math.max(end, charEnd);
-
+            for(int j = start;j<n;j++){
+                char c= s.charAt(j);
+                int cStart= first[c-'a'];
+                int cLast = last[c-'a'];
+                if(cStart < start) break;
+                end = Math.max(end, cLast);
                 if(j==end && end-start+1 < n){
-                    res= Math.max(res, end-start+1);
+                    res=Math.max(res, end-start+1);
                 }
             }
         }
-
         return res;
     }
 }
