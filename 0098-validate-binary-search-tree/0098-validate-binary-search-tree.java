85929c4cbba96409c78edd293b094324a81a062c
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-11 17:28:00 -0500

    Time: 0 ms (100%), Space: 44.5 MB (17.94%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,24 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        Deque<TreeNode> stack = new ArrayDeque<>();
-        Integer prev = null;
-
-        while (!stack.isEmpty() || root != null) {
-            while (root != null) {
-                stack.push(root);
-                root = root.left;
-            }
-            root = stack.pop();
-
-            // If next element in inorder traversal
-            // is smaller than the previous one
-            // that's not BST.
-            if (prev != null && root.val <= prev) {
-                return false;
-            }
-            prev = root.val;
-            root = root.right;
-        }
-        return true;
+        return validateBST(root, null, null);
+    }
+    public boolean validateBST(TreeNode root, Integer min, Integer max){
+        if(root==null) return true;
+        if(min !=null && root.val <= min || max!=null && root.val >= max) return false;
+        return validateBST(root.left, min, root.val) && validateBST(root.right, root.val, max);
     }
 }
