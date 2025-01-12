03706f2e19c4e7ae36da796f97a36c5d84e45c96
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-12 16:55:17 -0500

    Time: 21 ms (31.67%), Space: 51.4 MB (37.85%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,25 +1,19 @@
 class Solution {
-    int[][] memo ;
     public int numDistinct(String s, String t) {
-        if(s.equals(t)) return 1;
-        memo = new int[s.length()+1][t.length()+1];
-        for (int[] row : memo) {
-        Arrays.fill(row, -1);
-    }
-        return countSame(s, t, 0, 0,new StringBuilder());
-    }
-    public int countSame(String s, String t, int sIndex,int tIndex, StringBuilder sb ){
-        
-        if(tIndex == t.length()) return 1;
-        if(sIndex==s.length()) return 0;
-        if(memo[sIndex][tIndex]!=-1) return memo[sIndex][tIndex];
-        int cnt =0;
-        if(s.charAt(sIndex)==t.charAt(tIndex)){
-            cnt += countSame(s, t, sIndex+1, tIndex+1, sb);
+        int[][] dp = new int[s.length()+1][t.length()+1];
+        // Base case: An empty t is a subsequence of any prefix of s
+        for (int i = 0; i <= s.length(); i++) {
+            dp[i][0] = 1;
         }
-         cnt += countSame(s, t, sIndex+1, tIndex, sb);
-        
-
-        return memo[sIndex][tIndex]=cnt;
+        for(int i=1;i<=s.length();i++){
+            for(int j=1;j<=t.length();j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[s.length()][t.length()];
     }
 }
