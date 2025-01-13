447e7174f2dc8d605bf8e47fd2e99a463edf94f3
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-12 21:18:05 -0500

    Time: 633 ms (34.8%), Space: 41.3 MB (80.46%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,28 +1,15 @@
 class Solution {
-    int total =0;
+    int cnt=0;
     public int findTargetSumWays(int[] nums, int target) {
-        total = Arrays.stream(nums).sum();  
-        int[][] memo = new int[nums.length][2*total+1];
-        for (int[] row : memo) {
-            Arrays.fill(row, Integer.MIN_VALUE);
-        }
-        return findWays(nums, target, 0,0, memo);
-
+         getTargetSum(nums, target, 0, 0);
+         return cnt;
     }
-    public int findWays(int nums[], int target, int index, int sum, int[][] memo){
-        if( index == nums.length){
-            if(target ==sum)
-                return 1;
-            else return 0;
-        }else{
-            if(memo[index][sum+total] != Integer.MIN_VALUE){
-                return memo[index][sum+total];
-            }
-            int subtract = findWays(nums, target, index+1, sum-nums[index], memo);
-            int add = findWays(nums, target, index+1, sum+nums[index], memo);
-            memo[index][sum+total] = add+subtract;
-            return memo[index][sum+total];
+    public void getTargetSum(int[] nums, int target, int index, int curSum){
+        if(index==nums.length){
+            if(curSum==target) cnt++;
+            return;
         }
-        
-}
+        getTargetSum(nums, target, index+1, curSum+nums[index]);
+        getTargetSum(nums, target, index+1, curSum-nums[index]);
+    }
 }
