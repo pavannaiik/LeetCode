521a2c843fb5ba6f0e071c30ad3af7cb578b6a09
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-13 15:48:04 -0500

    Time: 10 ms (88.15%), Space: 41.7 MB (94.81%) - LeetHub

--- 1356-minimum-number-of-moves-to-make-palindrome/1356-minimum-number-of-moves-to-make-palindrome.java
+++ 1356-minimum-number-of-moves-to-make-palindrome/1356-minimum-number-of-moves-to-make-palindrome.java
@@ -1,41 +1,36 @@
 class Solution {
     public int minMovesToMakePalindrome(String s) {
         char[] ch = s.toCharArray();
-        int left = 0, right = ch.length - 1;
-        int moves = 0;
-
-        while (left < right) {
-            if (ch[left] == ch[right]) {
+        int n = s.length();
+        int left=0, right=n-1;
+        int res=0;
+        while(left < right){
+            if(ch[left]==ch[right]){
                 left++;
                 right--;
-            } else {
-                int k = right;
-                while (k > left && ch[k] != ch[left]) {
-                    k--;
+            }else{
+                int tmp = right;
+                while(tmp > left && ch[tmp]!=ch[left]){
+                    tmp--;
                 }
-
-                if (k == left) {
-                    // No matching character found, swap with next character
-                    swap(ch, left, left + 1);
-                    moves++;
-                } else {
-                    // Move the matching character to the right position
-                    while (k < right) {
-                        swap(ch, k, k + 1);
-                        moves++;
-                        k++;
+                if(tmp==left){
+                    swap(ch, left, left+1);
+                    res++;
+                }else{
+                    while(tmp< right){
+                        swap(ch, tmp, tmp+1);
+                        res++;
+                        tmp++;
                     }
                     left++;
                     right--;
                 }
             }
         }
-
-        return moves;
+        return res;
     }
-
-    private void swap(char[] ch, int i, int j) {
-        char temp = ch[i];
+    public void swap(char[] ch, int i, int j){
+       char temp = ch[i];
         ch[i] = ch[j];
         ch[j] = temp;
     }
