e225fda9eef83d9f7463354048a5cbca3bc1b52c
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-14 18:28:41 -0500

    Time: 0 ms (100%), Space: 40.8 MB (86.23%) - LeetHub

---
+++ 1022-unique-paths-iii/1022-unique-paths-iii.java
@@ -0,0 +1,39 @@
+class Solution {
+    int ans=0;
+    int emptyCells=0;
+    public int uniquePathsIII(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int start=0, end=0;
+        int cell=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    start=i;
+                    end=j;
+                }else if(grid[i][j]==0){
+                    emptyCells++;
+                }
+            }
+        }
+        dfs(grid, start,end, n, m,-1);
+        return ans;
+    }
+    public void dfs(int[][] grid, int i, int j, int n, int m, int count){
+        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]==-1){
+            return;
+        }
+        if(grid[i][j]==2){
+            if(emptyCells==count){
+                ans++;
+            }
+            return ;
+        }
+        grid[i][j] = -1;
+        dfs(grid, i,j+1, n, m, count+1);
+        dfs(grid, i,j-1, n, m, count+1);
+        dfs(grid, i+1,j, n, m, count+1);
+        dfs(grid, i-1,j, n, m, count+1);
+        grid[i][j] = 0;
+    }
+}
