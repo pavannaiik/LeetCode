fe995341d22da15c9acb71bb92d58dbbf6c688da
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-16 14:14:12 -0500

    Time: 13 ms (23.98%), Space: 44.6 MB (56.28%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -3,7 +3,6 @@ class Solution {
         Set<String>set = new HashSet<>();
         for(int i=0;i<9;++i){
             for(int j=0;j<9;++j){
-                
                 char num= board[i][j];
                 if(num!='.'){
                     if(!set.add(num + "seen at row " + i) || !set.add(num + "seen at col "+ j) || !set.add(num + "seen at box " + i/3 + "-" + j/3))
