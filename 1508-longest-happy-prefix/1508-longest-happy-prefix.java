dd9730f8c568c94969899a771d31bc28b3be57d6
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-23 18:43:10 -0500

    Time: 5 ms (100%), Space: 45.6 MB (33.8%) - LeetHub

--- 1508-longest-happy-prefix/1508-longest-happy-prefix.java
+++ 1508-longest-happy-prefix/1508-longest-happy-prefix.java
@@ -1,25 +1,31 @@
 class Solution {
-    public String longestPrefix(String str) {
-        int n = str.length();
-        int[] lps = new int[n];
-        int len = 0; // Length of the previous longest prefix-suffix
-        int i = 1;
+public String longestPrefix(String s) {
+    // Convert the string into a character array for efficient access
+    char[] chars = s.toCharArray();
+    int n = chars.length;
 
-        while (i < n) {
-            if (str.charAt(i) == str.charAt(len)) {
-                len++;
-                lps[i] = len;
-                i++;
+    // LPS array to store the lengths of the longest prefix-suffix at each position
+    int[] lps = new int[n];
+    int length = 0; // Tracks the current length of the matching prefix-suffix
+
+    // Start building the LPS array from the second character
+    for (int i = 1; i < n; ) {
+        if (chars[i] == chars[length]) {
+            length++;       // Increase length of the prefix-suffix match
+            lps[i] = length; // Store the length in the LPS array
+            i++;            // Move to the next character
+        } else {
+            if (length != 0) {
+                length = lps[length - 1]; // Fall back to the previous LPS
             } else {
-                if (len != 0) {
-                    len = lps[len - 1];
-                } else {
-                    lps[i] = 0;
-                    i++;
-                }
+                lps[i] = 0; // No match, set LPS as 0
+                i++;        // Move to the next character
             }
         }
-
-        return str.substring(0,lps[n-1]);
     }
+
+    // The longest prefix which is also a suffix is determined by lps[n - 1]
+    return s.substring(0, lps[n - 1]);
+}
+
 }
