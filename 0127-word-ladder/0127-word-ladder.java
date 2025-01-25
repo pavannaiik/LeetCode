1c5d71d2170cfe434602fc1f469a113bbac1b62c
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-25 14:58:18 -0500

    Time: 1291 ms (7.33%), Space: 57.2 MB (5.05%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,37 +1,43 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Map<String, List<String>> map = new HashMap<>();
-        wordList.forEach(word -> {
-            for (int i = 0; i < word.length(); i++) {
-                String newWord = word.substring(0, i) + '*' + word.substring(i + 1);
-                if(!map.containsKey(newWord)){
-                    map.put(newWord, new ArrayList<>());
+        Map<String, List<String>>map = new HashMap<>();
+        for(String word:wordList){
+            int len= word.length();
+            for(int i=0;i<len;i++){
+                String temp = word.substring(0, i)+"*"+word.substring(i+1);
+                if(!map.containsKey(temp)){
+                    map.put(temp, new ArrayList<>());
                 }
-                map.get(newWord).add(word);
-            }
-        });
-
-        Queue<Pair<String, Integer>> queue = new LinkedList<>();
-        queue.add(new Pair<>(beginWord, 1));
-        Set<String> visited = new HashSet<>();
-        visited.add(beginWord);
-
-        while (!queue.isEmpty()) {
-            Pair<String, Integer> node = queue.poll();
-            String word = node.getKey();
-            int level = node.getValue();
-
-            for (int i = 0; i < word.length(); i++) {
-                String newWord = word.substring(0, i) + '*' + word.substring(i + 1);
-                for (String adjacentWord : map.getOrDefault(newWord, new ArrayList<>())) {
-                    if (adjacentWord.equals(endWord)) return level + 1;
-                    if (!visited.contains(adjacentWord)) {
-                        visited.add(adjacentWord);
-                        queue.add(new Pair<>(adjacentWord, level + 1));
-                    }
-                }
+                map.get(temp).add(word);
             }
         }
+        Queue<String>queue = new LinkedList<>();
+        queue.offer(beginWord);
+        Set<String>visited = new HashSet<>();
+        int level=1;
+        while(!queue.isEmpty()){
+            int curLen = queue.size();
+            for(int j=0;j<curLen;j++){
+                String curString = queue.poll();
+                int len = curString.length();
+                for(int i=0;i<len;i++){
+                    String temp = curString.substring(0, i)+"*"+curString.substring(i+1);
+                    List<String>words = map.get(temp);
+                    if(words==null) continue;
+                    for(String str:words){
+                        if(endWord.equals(str)){
+                            return level+1;
+                        }
+                        if(!visited.contains(str)){
+                            queue.offer(str);
+                        }
+                    }
+                }
+                visited.add(curString);
+            }
+            level++;
+        }
         return 0;
+
     }
 }
