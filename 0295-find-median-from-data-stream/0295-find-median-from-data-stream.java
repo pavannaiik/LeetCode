3f5e7d56b518e4348b0562d2abd1e0cf63f9a2a9
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-25 22:38:53 -0500

    Time: 93 ms (72.02%), Space: 73.5 MB (5.05%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,27 +1,40 @@
 class MedianFinder {
-    List<Integer> list;
+
+    private final PriorityQueue<Integer> left = new PriorityQueue<>((a,b)->b-a);
+    private final PriorityQueue<Integer> right = new PriorityQueue<>();
+    private Integer middle = null;
 
     public MedianFinder() {
-        list = new ArrayList<>();
     }
     
     public void addNum(int num) {
-        // Insert the number into the list and maintain sorted order
-        int pos = Collections.binarySearch(list, num);
-        if (pos < 0) pos = -(pos + 1);
-        list.add(pos, num);
+        if (middle != null){
+            left.add(Math.min(num, middle));
+            right.add(Math.max(num, middle));
+            middle = null;
+        }else if (left.isEmpty() && right.isEmpty()){
+            middle = num;
+        } else if (num < left.peek()){
+            middle = left.remove();
+            left.add(num);
+        }else if (num > right.peek()){
+            middle = right.remove();
+            right.add(num);
+        }else{
+            middle = num;
+        }
+
+//        System.out.printf("After add %d, left=%s, mid=%s, right=%s\n", num, left, middle, right);
     }
     
     public double findMedian() {
-        int n = list.size();
-        if (n % 2 == 0) {
-            return (list.get(n / 2) + list.get(n / 2 - 1)) / 2.0;
-        } else {
-            return list.get(n / 2);
+        if (middle == null){
+            return (left.peek() + right.peek()) / 2d;
+        }else{
+            return middle;
         }
     }
 }
-
 
 /**
  * Your MedianFinder object will be instantiated and called as such:
