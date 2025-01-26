c8b85ab7aa9ae8c5aa7ed1d0530d8cc7e53c9999
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-01-25 20:08:02 -0500

    Time: 1071 ms (7.38%), Space: 47 MB (13.15%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,34 +1,32 @@
 class Solution {
-    private int n;
-    private HashSet<Character>set = new HashSet<>();
     public int countPalindromicSubsequence(String s) {
-        n = s.length();
-        int ans =0;
-        for(int c = 97;c<=122;c++){
-            int i =0, j=n-1;
-            int f =-1, l=-1;
-            while(i<n){
-                if(s.charAt(i)==(char)c){
-                    f = i;
-                    break;
-                }
-                i++;
-            }
-            while(j>=0){
-                if(s.charAt(j)==(char)c){
-                    l=j;
-                    break;
-                }
-                j--;
-            }
-            set.clear();
-            if( f!=-1 && l!=-1 &&l-f>1){
-                for (int k = f + 1; k < l; k++) {
-                    set.add(s.charAt(k));
-                }
-                ans += (set.size());
-            }
+        int n = s.length();
+        int[] left = new int[26]; // Tracks counts of characters seen so far
+        int[] right = new int[26]; // Tracks counts of characters remaining
+        Set<String> uniquePalindromes = new HashSet<>();
+
+        // Initialize right array with the frequency of each character in the string
+        for (char c : s.toCharArray()) {
+            right[c - 'a']++;
         }
-        return ans;
+
+        // Iterate through the string
+        for (int i = 0; i < n; i++) {
+            char middle = s.charAt(i);
+            right[middle - 'a']--; // Remove current character from right frequency
+
+            // Check for potential palindromes with the current character as the middle
+            for (int j = 0; j < 26; j++) {
+                if (left[j] > 0 && right[j] > 0) {
+                    String palindrome = "" + (char) (j + 'a') + middle + (char) (j + 'a');
+                    uniquePalindromes.add(palindrome);
+                }
+            }
+
+            // Add current character to left frequency
+            left[middle - 'a']++;
+        }
+
+        return uniquePalindromes.size();
     }
 }
