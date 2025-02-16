afe701b0cc1b92da16576db973e4a76809171728
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-02-16 18:07:57 -0500

    Time: 6 ms (41.44%), Space: 21.5 MB (91.97%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
+    bool isValid(vector<vector<char>>& board, int row, int col, char ch){
+        int n = board.size();
+        int m = board[0].size();
+        
+        for(int p=0;p<m;p++){
+            if(p!=col){
+                if(ch==board[row][p]) return false;
+            }
+            if(p!=row){
+                if(ch==board[p][col]) return false;
+            }
+        }
+        int startRow = (row/3)*3;
+        int startCol = (col/3)*3;
+        for (int i = startRow; i < startRow + 3; i++) 
+            for (int j = startCol; j < startCol + 3; j++) 
+                if ((i != row || j != col) && board[i][j] == ch) 
+                    return false;
+        return true;
+    }
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_set<string>s;
         int n=board.size(),m=board[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 char ch = board[i][j];
                 if(ch!='.'){
-                    string rowCheck = to_string(ch)+"seen at row "+to_string(i);
-                    string colCheck = to_string(ch)+"seen at col "+to_string(j);
-                    string cellCheck = to_string(ch)+"seen at cell "+to_string(i/3)+"-"+to_string(j/3);
-
-                    if(!s.insert(rowCheck).second || !s.insert(colCheck).second|| !s.insert(cellCheck).second){
+                    if(!isValid(board, i , j, ch)){
                         return false;
                     }
                 }
