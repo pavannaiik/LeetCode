3b11f55eefd0baebdf8c3a40755597c0e71f5706
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-02-23 00:58:47 -0500

    Time: 3 ms (91.67%), Space: 41.9 MB (68.15%) - LeetHub

--- 2186-count-vowel-substrings-of-a-string/2186-count-vowel-substrings-of-a-string.java
+++ 2186-count-vowel-substrings-of-a-string/2186-count-vowel-substrings-of-a-string.java
@@ -4,15 +4,20 @@ class Solution {
         int n = word.length();
         int count =0;
         int left=0;
+        int len =0;
         for(int i=0;i<n;i++){
             char c = word.charAt(i);
             if(c=='a' || c=='e'||c=='o'||c=='i'||c=='u'){
+                if(!map.containsKey(c)){
+                    len++;
+                }
                 map.put(c,i);
             }else{
                 map.clear();
                 left=i+1;
+                len=0;
             }
-            if(map.size()==5){
+            if(len==5){
                 int minCount = Integer.MAX_VALUE;
                 for(int val:map.values()){
                     minCount = Math.min(minCount, val);
