0f11ef3fc57e3c8e328a09e65d57badefaa9a494
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-02-23 01:42:19 -0500

    Time: 63 ms (98.53%), Space: 76.2 MB (12.35%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
@@ -1,18 +1,36 @@
+
 class Solution {
-        public int maxEvents(int[][] A) {
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        Arrays.sort(A, (a, b) -> Integer.compare(a[0], b[0]));
-        int i = 0, res = 0, n = A.length;
-        for (int d = 1; d <= 100000; ++d) {
-            while (!pq.isEmpty() && pq.peek() < d)
-                pq.poll();
-            while (i < n && A[i][0] == d)
-                pq.offer(A[i++][1]);
-            if (!pq.isEmpty()) {
-                pq.poll();
-                ++res;
+    public int maxEvents(int[][] events) {
+        // Sort events by start time, if same start time then by end time
+        Arrays.sort(events, (a, b) -> Integer.compare(a[0], b[0]));
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(); // Min-Heap to store event end times
+        int i = 0, n = events.length, maxDays = 0, attended = 0;
+
+        // Find max day among all events
+        for (int[] event : events) {
+            maxDays = Math.max(maxDays, event[1]);
+        }
+
+        // Iterate over all possible days
+        for (int day = 1; day <= maxDays; day++) {
+            // Add all events that start on this day to the min-heap
+            while (i < n && events[i][0] == day) {
+                minHeap.add(events[i][1]);
+                i++;
+            }
+
+            // Remove expired events
+            while (!minHeap.isEmpty() && minHeap.peek() < day) {
+                minHeap.poll();
+            }
+
+            // Attend the event that ends the earliest
+            if (!minHeap.isEmpty()) {
+                minHeap.poll();
+                attended++;
             }
         }
-        return res;
+        return attended;
     }
 }
