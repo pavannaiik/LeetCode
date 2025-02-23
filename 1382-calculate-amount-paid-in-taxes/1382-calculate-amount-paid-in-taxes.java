d604aa7b7254d167a1da797b48f094a2e250f919
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-02-23 01:17:08 -0500

    Time: 1 ms (20.69%), Space: 44.8 MB (51.72%) - LeetHub

---
+++ 1382-calculate-amount-paid-in-taxes/1382-calculate-amount-paid-in-taxes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double calculateTax(int[][] brackets, int income) {
+        double totalTax =0;
+        int n = brackets.length;
+        for(int i=0;i<n;i++){
+            if(i==0){
+                totalTax = (Math.min(brackets[i][0], income)*((double)brackets[i][1]/100.0));
+                income -= brackets[i][0];
+                if(income <=0) break;
+            }else{
+                totalTax += (Math.min(income, brackets[i][0]-brackets[i-1][0])*((double)brackets[i][1]/100.0));
+                income -= (brackets[i][0]-brackets[i-1][0]);
+                if(income <=0) break;
+            }
+        }
+        return totalTax;
+    }
+}
