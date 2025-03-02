29c9fa4952fa0450059003afaa91f09df97f1f3f
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-03-02 12:39:10 -0500

    Time: 1 ms (65.7%), Space: 42 MB (25.88%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,14 +3,13 @@ class Solution {
         StringBuilder sb = new StringBuilder(strs[0]);
         int n = strs.length;
         for(int i=1;i<n;i++){
-            StringBuilder temp = new StringBuilder();
-            int size = Math.min(sb.length(), strs[i].length());
-            int j=0;
-            while(j< size && sb.charAt(j)==strs[i].charAt(j)){
-                temp.append(sb.charAt(j));
-                j++;
+            String t = strs[i];
+            int j = Math.min(sb.length(), t.length());
+            while( j >=0 && !sb.substring(0,j).equals(t.substring(0,j))){
+                j--;
             }
-            sb = temp;
+            if(j>=0)
+            sb = new StringBuilder(sb.substring(0,j));
         }
         return sb.toString();
     }
