d4d60cd7c8dbae1a959530b403f09f2db28a3100
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-03-11 00:03:47 -0400

    Time: 4 ms (75.5%), Space: 43.2 MB (73.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())
-            return false;
-       int[] arr=new int[26];
-       Arrays.fill(arr,0);
-       for(int i=0;i<s.length();i++){
-           arr[s.charAt(i)-'a']++;
-           arr[t.charAt(i)-'a']--;
-       }
-       for(int i:arr){
-           if(i!=0) return false;
-       }
-       return true;
+        // if (s.length() > 300 && s.charAt(0) == 'h') {
+        //     return true;
+        // } else if (s.length() > 256 && (s.charAt(0) == 'h' || s.charAt(0) == 'a')) {
+        //     return false;
+        // }
+        // if (s.length() != t.length()) return false;
+    
+        int cnt[] = new int[26]; // 26 chars in alphabet
+
+        for(int i=0; i<s.length(); i++) cnt[s.charAt(i) - 'a']++;
+        for(int i=0; i<t.length(); i++) cnt[t.charAt(i) - 'a']--;        
+        for (int e : cnt) {
+            if (e != 0) return false;
+        }
+        return true;
     }
 }
