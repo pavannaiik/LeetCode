f39a31b1da7048fc0bc0a45730be99c613bc9d84
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-03-11 18:51:00 -0400

    Time: 1 ms (99.9%), Space: 44.8 MB (52.89%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,21 +1,27 @@
 class Solution {
+    int pos;
     public int evalRPN(String[] tokens) {
-        int ans=0;
-        Stack<Integer> stack = new Stack<>();
-        for(int i=0;i<tokens.length;i++){
-            String s = tokens[i];
-            if(s.equals("+") || s.equals("-")||s.equals("*")||s.equals("/")){
-                int a = stack.peek();
-                stack.pop();
-                int b= stack.peek();
-                stack.pop();
-                if(s.equals("+")) stack.push(b+a);
-                else if(s.equals("-")) stack.push(b-a);
-                else if(s.equals("*")) stack.push(b*a);
-                else stack.push(b/a);
-            }else stack.push(Integer.parseInt(s));
+        this.pos = tokens.length-1;
+        return evaluate(tokens);
+    }
+
+    public int evaluate(String[] t){
+        String token = t[pos];
+        pos--;
+        if(token.equals("+")){
+            return evaluate(t) + evaluate(t); 
+        }else
+        if(token.equals("*")){
+            return evaluate(t) * evaluate(t);
+        }else 
+        if(token.equals("-")){
+            return -evaluate(t) + evaluate(t);
+        }else
+        if(token.equals("/")){
+            int right = evaluate(t);
+            return evaluate(t) / right;
+        }else{
+            return Integer.parseInt(token);
         }
-        return stack.peek();
-        
     }
 }
