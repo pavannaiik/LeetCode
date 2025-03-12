ddb636968ea85a80e2507e221f4817c08c592552
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-03-11 20:04:54 -0400

    Time: 2 ms (85.64%), Space: 48.2 MB (55.46%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,39 +10,29 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        
         ListNode slow = head;
-        ListNode fast = head.next;
+        ListNode fast = head;
         while(fast!=null && fast.next!=null){
             slow= slow.next;
             fast = fast.next.next;
         }
         
-        ListNode reversed = reverse(slow.next);
-        slow.next=null;
-        
-        ListNode curr = head;
-        while(curr!=null && reversed!=null){
-            ListNode nextCurr = curr.next;
-            curr.next=reversed;
-            ListNode nextReversed = reversed.next;
-            reversed.next = nextCurr;
-            curr = nextCurr;
-            reversed = nextReversed;
-        } 
-        
-        
-    }
-    
-    public ListNode reverse(ListNode node){
-        ListNode prev = null;
-        ListNode cur = node;
-        while(cur!=null){
-            ListNode temp = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = temp;
+        ListNode reversed  = reverse(slow.next, null);
+        slow.next = null;
+        while(reversed!=null){
+            ListNode temp = head.next;
+            ListNode tempRev = reversed.next;
+            head.next = reversed;
+            reversed.next = temp;
+            head = head.next.next;
+            reversed = tempRev;
         }
-        return prev;
+
+    }
+    public ListNode reverse(ListNode node, ListNode prev){
+        if(node == null) return prev;
+        ListNode cur = node.next;
+        node.next = prev;
+        return reverse(cur, node);
     }
 }
