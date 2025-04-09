a5b9c4369ba87af67591d9ae432dc8b821cd8532
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-09 19:20:41 -0400

    Time: 4 ms (75.52%), Space: 43 MB (90.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // if (s.length() > 300 && s.charAt(0) == 'h') {
-        //     return true;
-        // } else if (s.length() > 256 && (s.charAt(0) == 'h' || s.charAt(0) == 'a')) {
-        //     return false;
-        // }
-        // if (s.length() != t.length()) return false;
-    
-        int cnt[] = new int[26]; // 26 chars in alphabet
-
-        for(int i=0; i<s.length(); i++) cnt[s.charAt(i) - 'a']++;
-        for(int i=0; i<t.length(); i++) cnt[t.charAt(i) - 'a']--;        
-        for (int e : cnt) {
-            if (e != 0) return false;
+        
+if (s.length() != t.length()) {
+        return false;
+    }
+    int[] table = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+        table[s.charAt(i) - 'a']++;
+    }
+    for (int i = 0; i < t.length(); i++) {
+        table[t.charAt(i) - 'a']--;
+        if (table[t.charAt(i) - 'a'] < 0) {
+            return false;
         }
-        return true;
+    }
+    return true;
+        
     }
 }
