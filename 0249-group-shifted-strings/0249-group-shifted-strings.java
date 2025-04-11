4b125b613731c11a6b70cbaf7f5cb3f14c54257b
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-10 22:46:19 -0400

    Time: 2 ms (80.58%), Space: 43.3 MB (71.16%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.java
+++ 0249-group-shifted-strings/0249-group-shifted-strings.java
@@ -1,26 +1,28 @@
 class Solution {
     public List<List<String>> groupStrings(String[] strings) {
-        HashMap<String,List<String>>map = new HashMap<>();
-        List<List<String>>result = new ArrayList<>();
-        for(String s:strings){
-            StringBuilder sb = new StringBuilder();
-            char firstChar = s.charAt(0);
-            for(int i=0;i<s.length();i++){
-                char c = s.charAt(i);
-                int diff = c-firstChar;
-                if(diff<0){
-                    diff = diff+25+1;
-                }
-                sb.append(diff);
-                sb.append(',');
-            }
-            map.putIfAbsent(sb.toString(), new ArrayList<>());
-            map.get(sb.toString()).add(s);
+        HashMap<String, List<String>> map = new HashMap<>();
+        
+        for (String s : strings) {
+            // Compute a normalized shift key for each string
+            String key = getShiftKey(s);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(s);
         }
-        for(String key:map.keySet()){
-            result.add(map.get(key));
+        
+        // Return the groups as a list of lists
+        return new ArrayList<>(map.values());
+    }
+    
+    private String getShiftKey(String s) {
+        StringBuilder key = new StringBuilder();
+        char firstChar = s.charAt(0);
+        
+        for (char c : s.toCharArray()) {
+            // Use modulo arithmetic to handle the wrap-around from 'z' to 'a'
+            int diff = (c - firstChar + 26) % 26;
+            key.append(diff).append(',');
         }
-        return result;
-
+        
+        return key.toString();
     }
 }
