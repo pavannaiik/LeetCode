d429b161d3afc081c2d6691078609fd82371d048
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-10 22:39:17 -0400

    Time: 3 ms (42.29%), Space: 43.5 MB (44.87%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.java
+++ 0249-group-shifted-strings/0249-group-shifted-strings.java
@@ -1,56 +1,25 @@
 class Solution {
     public List<List<String>> groupStrings(String[] strings) {
-        List<List<String>>result = new ArrayList<>();
-        
         HashMap<String,List<String>>map = new HashMap<>();
-        HashMap<String,Integer>countMap = new HashMap<>();
-        for(String s: strings){
-            StringBuilder sb = new StringBuilder(s);
-            countMap.put(s, countMap.getOrDefault(s, 0)+1);
-            if(map.containsKey(s))continue;
-            map.put(s, new ArrayList<>());
-            for(int i=0;i<26;i++){
-                StringBuilder temp = new StringBuilder();
-                for(char c:sb.toString().toCharArray()){
-                    if(c=='z'){
-                        temp.append('a');
-                    }else{
-                        temp.append((char)(c + 1));
-                    }
+        List<List<String>>result = new ArrayList<>();
+        for(String s:strings){
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<s.length();i++){
+                char c = s.charAt(i);
+                int diff = c-s.charAt(0);
+                if(diff<0){
+                    diff = diff+25+1;
                 }
-                sb = temp;
-                map.get(s).add(temp.toString());
+                sb.append(diff);
+                sb.append(',');
             }
+            map.putIfAbsent(sb.toString(), new ArrayList<>());
+            map.get(sb.toString()).add(s);
         }
-        HashMap<String,List<String>>groupMap = new HashMap<>();
-        HashSet<String>visited = new HashSet<>();
-        for(String key: map.keySet()){
-            if (visited.contains(key))
-                continue;
-
-            List<String>keys = map.get(key);
-            groupMap.putIfAbsent(key, new ArrayList<>());
-            int count = countMap.get(key);
-            while(count-- >0){
-                 groupMap.get(key).add(key);
-            }
-            visited.add(key);
-            for(String s: keys){
-                if(map.containsKey(s) && !visited.contains(s)){
-                    int cnt = countMap.get(s);
-                    while(cnt-- >0){
-                        groupMap.get(key).add(s);
-                    }
-                    
-                    visited.add(s);
-                }
-            }
+        for(String key:map.keySet()){
+            result.add(map.get(key));
         }
-        for(String key: groupMap.keySet()){
-            if(groupMap.get(key).size()>0)
-            result.add(groupMap.get(key));
-        }
-
         return result;
+
     }
 }
