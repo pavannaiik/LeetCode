a0dfff64fc17321b66522f067f299cb19b407d78
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-11 21:33:32 -0400

    Time: 5 ms (2.08%), Space: 43.4 MB (23.76%) - LeetHub

---
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    double closestValue = Double.MAX_VALUE;
+    int val = Integer.MAX_VALUE;
+    public int closestValue(TreeNode root, double target) {
+        getClosest(root, target);
+        return val;
+    }
+    public void getClosest(TreeNode root, double target){
+        if(root==null) return;
+        double diff = (double)(root.val) - target;
+       // closestValue = Math.min(closestValue, Math.abs(diff));
+        if(closestValue > Math.abs(diff)){
+            closestValue = Math.abs(diff);
+            val = root.val;
+        }
+        if(closestValue == Math.abs(diff)){
+            val = Math.min(val, root.val);
+        }
+        System.out.println(closestValue);
+        if(diff > 0){
+            getClosest(root.left, target);
+        }else{
+            getClosest(root.right, target);
+        }
+    }
+}
