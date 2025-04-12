3737bdd1343eb0dd2f72fb7f7c185d579bad384f
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-12 17:03:06 -0400

    Time: 4 ms (72.27%), Space: 44.2 MB (90.21%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,44 +10,21 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        return mergeLists(lists, 0, lists.length-1);
-    }
-    public ListNode mergeLists(ListNode[] lists, int left, int right){
-        if(left==right){
-            return lists[left];
-        }
-        if(left > right ) return null;
-
-        int mid = left+(right-left)/2;
-        ListNode leftNode = mergeLists(lists, left, mid);
-        ListNode rightNode = mergeLists(lists, mid+1, right);
-        return merge(leftNode, rightNode);
-        
-        
-    }
-    public ListNode merge(ListNode leftNode, ListNode rightNode){
+        PriorityQueue<ListNode>pq = new PriorityQueue<>((a,b)->a.val-b.val);
         ListNode cur = new ListNode(0);
         ListNode result = cur;
-        while(leftNode!=null && rightNode!=null){
-            if(leftNode.val < rightNode.val){
-                cur.next = new ListNode(leftNode.val);
-                leftNode = leftNode.next;
-            }else{
-                cur.next = new ListNode(rightNode.val);
-                rightNode = rightNode.next;
-            }
-            cur=cur.next;
+        for(ListNode list: lists){
+            if(list !=null)
+                pq.add(list);
         }
-        while(leftNode!=null){
-            cur.next = new ListNode(leftNode.val);
-            leftNode = leftNode.next;
-            cur=cur.next;
-        }
-         while(rightNode!=null){
-            cur.next = new ListNode(rightNode.val);
-            rightNode = rightNode.next;
-            cur=cur.next;
+        while(!pq.isEmpty()){
+            ListNode temp = pq.poll();
+            cur.next = new ListNode(temp.val);
+            ListNode nextTemp = temp.next;
+            if(nextTemp !=null) pq.add(nextTemp);
+            cur = cur.next;
         }
         return result.next;
+
     }
 }
