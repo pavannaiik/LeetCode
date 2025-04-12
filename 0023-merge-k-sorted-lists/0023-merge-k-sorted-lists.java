77548651eb4a4e6fd7f3b6d510131f9e3abf5b93
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-12 16:43:31 -0400

    Time: 2 ms (84.28%), Space: 45.6 MB (5.14%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,20 +10,45 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode>pq = new PriorityQueue<>((a,b)->a.val-b.val);
-        ListNode result = new ListNode(0);
-        ListNode ans = result;
-        for(ListNode list:lists){
-            if(list!=null)
-            pq.add(list);
+        return mergeLists(lists, 0, lists.length-1);
+    }
+    public ListNode mergeLists(ListNode[] lists, int left, int right){
+        if(left==right){
+            return lists[left];
         }
-        while(!pq.isEmpty()){
-            ListNode cur = pq.poll();
-            ListNode next = cur.next;
-            result.next = new ListNode(cur.val);
-            result = result.next;
-            if(next!=null) pq.add(next);
+        if(left<right){
+            int mid = left+(right-left)/2;
+        ListNode leftNode = mergeLists(lists, left, mid);
+        ListNode rightNode = mergeLists(lists, mid+1, right);
+        return merge(leftNode, rightNode);
+        }else{
+            return null;
         }
-        return ans.next;
+        
+    }
+    public ListNode merge(ListNode leftNode, ListNode rightNode){
+        ListNode cur = new ListNode(0);
+        ListNode result = cur;
+        while(leftNode!=null && rightNode!=null){
+            if(leftNode.val < rightNode.val){
+                cur.next = new ListNode(leftNode.val);
+                leftNode = leftNode.next;
+            }else{
+                cur.next = new ListNode(rightNode.val);
+                rightNode = rightNode.next;
+            }
+            cur=cur.next;
+        }
+        while(leftNode!=null){
+            cur.next = new ListNode(leftNode.val);
+            leftNode = leftNode.next;
+            cur=cur.next;
+        }
+         while(rightNode!=null){
+            cur.next = new ListNode(rightNode.val);
+            rightNode = rightNode.next;
+            cur=cur.next;
+        }
+        return result.next;
     }
 }
