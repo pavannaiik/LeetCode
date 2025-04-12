201cc9dbd8e06be25cd90d6358a5eead8e5f3610
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-11 20:41:14 -0400

    Time: 17 ms (70.86%), Space: 45.3 MB (72.7%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,47 +1,38 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer>stack = new Stack<>();
-        char operator = '#';
-        int curVal = -1;
+        int result =0;
+        int curVal =-1;
+        int prevVal =0;
+        char operator ='#';
         int i=0, j= s.length();
-        while(i < j){
+        while(i<j){
             char ch = s.charAt(i);
-            if(Character.isWhitespace(ch) ) {
+            if(Character.isWhitespace(ch)|| !Character.isDigit(ch)){
+                if(!Character.isWhitespace(ch)) operator=ch;
                 i++;
                 continue;
-            }
-            if(Character.isDigit(ch)){
-                int t =0;
-                while(i< j && Character.isDigit(s.charAt(i))){
-                    t = t * 10 + s.charAt(i)-'0';
+            }else {
+                int t = 0;
+                while(i < j && Character.isDigit(s.charAt(i))){
+                    t = t*10 + s.charAt(i)-'0';
                     i++;
                 }
                 curVal = t;
-                i--;
-            }else{
-                operator = ch;
-                i++;
-                continue;
-            }
 
-            if(operator == '#' && curVal !=-1){
-                stack.push(curVal);
-            }else if(operator=='+'){
-                stack.push(curVal);
-            }else if(operator == '-'){
-                stack.push(-curVal);
-            }else if(operator=='*' ){
-                int top = stack.pop();
-                stack.push(top * curVal);
-            }else {
-                int top = stack.pop();
-                stack.push(top / curVal);
+                if(operator == '+' || operator=='-'){
+                    result += prevVal;
+                    if(operator=='-') curVal = -1 * curVal;
+
+                }else if(operator!='#'){
+                    if(operator=='*') curVal = curVal*prevVal;
+                    else curVal = curVal==0?0:prevVal/curVal;
+                }
+
+                prevVal = curVal;
+                curVal =-1;
+                operator = '#';
             }
-            curVal =0;
-            i++;
         }
-        int result = 0;
-        while(!stack.isEmpty()) result += stack.pop();
-        return result;
+        return result+prevVal;
     }
 }
