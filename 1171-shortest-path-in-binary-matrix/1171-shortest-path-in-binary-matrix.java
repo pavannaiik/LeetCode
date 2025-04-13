f12cc321675d18ee965ddcee3770378e65c60884
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-13 12:32:10 -0400

    Time: 41 ms (8.56%), Space: 45.7 MB (54.38%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
@@ -2,23 +2,54 @@
 
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-         int[][] dir = {{0,1},{1,0},{0,-1},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
+        int[][] dir = {{0,1},{1,0},{0,-1},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
         int n = grid.length, m = grid[0].length;
+
         if(grid[0][0] != 0 || grid[n-1][m-1] != 0) return -1;
-        Queue<int[]>queue = new LinkedList<>();
-        queue.add(new int[]{0, 0, 0});
-        while(!queue.isEmpty()){
-            int[] cur = queue.poll();
-            int r = cur[0], c =cur[1], val = cur[2];
-            if(r==n-1 && c==m-1 ) return val+1;
-            for(int i=0;i<8;i++){
-                int x = dir[i][0]+r;
-                int y = dir[i][1]+c;
-                if(x <0 || y <0 || x > grid.length-1 || y >grid[0].length-1||grid[x][y]!=0) continue;
-                grid[x][y]=-1;
-                queue.add(new int[]{x,y,val+1});
+
+        Queue<Node> queue = new LinkedList<>();
+        List<int[]> startPath = new ArrayList<>();
+        startPath.add(new int[]{0, 0});
+        queue.add(new Node(0, 0, startPath));
+        grid[0][0] = -1;
+
+        while(!queue.isEmpty()) {
+            Node cur = queue.poll();
+            int r = cur.row, c = cur.col;
+            List<int[]> path = cur.path;
+
+            // Check if we've reached the bottom-right cell
+            if(r == n - 1 && c == m - 1) {
+                System.out.println("Shortest path length: " + path.size());
+                System.out.println("Path:");
+                for (int[] p : path) {
+                    System.out.println(Arrays.toString(p));
+                }
+                return path.size();
+            }
+
+            for(int i = 0; i < 8; i++) {
+                int x = r + dir[i][0];
+                int y = c + dir[i][1];
+                if(x < 0 || y < 0 || x >= n || y >= m || grid[x][y] != 0) continue;
+
+                grid[x][y] = -1; // Mark as visited
+                List<int[]> newPath = new ArrayList<>(path);
+                newPath.add(new int[]{x, y});
+                queue.add(new Node(x, y, newPath));
             }
         }
+
         return -1;
+    }
+
+    static class Node {
+        int row, col;
+        List<int[]> path;
+        Node(int row, int col, List<int[]> path) {
+            this.row = row;
+            this.col = col;
+            this.path = path;
+        }
     }
 }
