e523f3f8b54e84d51194e4fdf59c5feded0dad52
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-14 18:43:55 -0400

    Time: 4 ms (32.69%), Space: 43.7 MB (29.63%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -4,39 +4,19 @@ class Solution {
         int i=n-1, j=m-1;
         int carry =0;
         StringBuilder sb = new StringBuilder();
-        while(i>=0 && j>=0){
-            char ch = num2.charAt(i);
-            char c = num1.charAt(j);
-            int num = (ch-'0' )+ (c -'0') + carry;
-            if(num > 9){
-                carry = num/10;
-                num = num%10;
-            }else carry =0;
-            sb.insert(0,(char)(num+'0'));
-            i--;
-            j--;
+        while(i>=0 || j>=0 || carry>0){
+            int num = carry;
+            if(i >=0 ){
+                num += num2.charAt(i)-'0';
+                i--;
+            }
+            if(j >=0){
+                num += num1.charAt(j)-'0';
+                j--;
+            }
+            carry = num/10;
+            sb.insert(0, num%10);
         }
-        while(i>=0){
-            char ch = num2.charAt(i);
-            int num = (ch-'0')  + carry;
-            if(num > 9){
-                carry = num/10;
-                num = num%10;
-            }else carry =0;
-            sb.insert(0,(char)(num+'0'));
-            i--;
-        }
-        while(j>=0){
-            char ch = num1.charAt(j);
-            int num = (ch-'0')  + carry;
-            if(num > 9){
-                carry = num/10;
-                num = num%10;
-            }else carry =0;
-            sb.insert(0,(char)(num+'0'));
-            j--;
-        }
-        if(carry > 0) sb.insert(0, carry);
         return sb.toString();
     }
 }
