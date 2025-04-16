ab123f6c89159bc77098411d07ca1b2800a85333
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-16 01:21:38 -0400

    Time: 2 ms (90.93%), Space: 41.8 MB (63.47%) - LeetHub

--- 0756-pour-water/0756-pour-water.java
+++ 0756-pour-water/0756-pour-water.java
@@ -3,9 +3,17 @@ class Solution {
         int cur = k;
         int n = heights.length;
         for(int i=0; i < volume;i++){
-            while(cur>0 && heights[cur] >= heights[cur-1]) cur--;
-            while(cur < n-1 && heights[cur]>= heights[cur+1]) cur++;
-            while(cur>k && heights[cur]==heights[cur-1]) cur--;
+            //go all the way left 
+            while( cur >0 && heights[cur]>=heights[cur-1]){
+                cur--;
+            }
+            //go all the way to right
+            while(cur < n-1 && heights[cur]>= heights[cur+1]){
+                cur++;
+            }
+            while(cur > k && heights[cur]==heights[cur-1]){
+                cur--;
+            }
             heights[cur]+=1;
         }
         return heights;
