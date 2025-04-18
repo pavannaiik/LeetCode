84148f04321fc78ec4003ee67505bd3b2a0f55e6
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-18 00:44:51 -0400

    Time: 2 ms (84.67%), Space: 46.2 MB (19.66%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,11 +1,15 @@
 class Solution {
   public void moveZeroes(int[] nums) {
-    int i = 0;
-    for ( int num : nums)
-      if (num != 0)
-        nums[i++] = num;
-
-    while (i < nums.length)
-      nums[i++] = 0;
+    int i=0, si=0;
+    int n = nums.length;
+    while(si < n ){
+        if(nums[si]!=0){
+            int t = nums[si];
+            nums[si]= nums[i];
+            nums[i]=t;
+            i++;
+        }
+        si++;
+    }
   }
 }
