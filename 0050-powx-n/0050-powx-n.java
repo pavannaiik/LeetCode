2ff9f460bd31f9e841fafc1109571b782c5e78f0
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-19 15:52:43 -0400

    Time: 0 ms (100%), Space: 42.4 MB (46.32%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,14 +1,13 @@
 class Solution {
     public double myPow(double x, int n) {
-        
-        return myPower(x,(long)n);
+        return myPower(x, (long)n);
     }
-    public double myPower(double x,long n){
+    public double myPower(double x, long n){
         if(n==0) return 1;
         if(n<0){
-            return 1.0 / myPower(x,-n);
+            return 1.0/myPower(x, -n);
         }
-        if(n%2 ==0){
+        if(n%2==0){
             return myPower(x*x, n/2);
         }else{
             return x*myPower(x*x,(n-1)/2);
