9f15b8f4e4d89facabffc1c6b89b48dbd11e3dfd
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-18 23:57:01 -0400

    Time: 26 ms (5.27%), Space: 56.1 MB (90.66%) - LeetHub

--- 1306-minimum-absolute-difference/1306-minimum-absolute-difference.java
+++ 1306-minimum-absolute-difference/1306-minimum-absolute-difference.java
@@ -3,18 +3,31 @@ class Solution {
         List<List<Integer>>result = new ArrayList<>();
         Arrays.sort(arr);
         int n = arr.length;
-        int minDiff = arr[1]-arr[0];
-
-        for(int i=1;i<n;i++){
-            if(minDiff == arr[i]-arr[i-1]){
-                result.add(List.of(arr[i-1],arr[i]));
-            }else if(minDiff > arr[i]-arr[i-1]){
-                result.clear();
-                    minDiff = arr[i]-arr[i-1];
-                    result.add(List.of(arr[i-1],arr[i]));
-                
-            }
-        }
-        return result;
+       int minNum =arr[0], maxNum = arr[0];
+       for(int num:arr){
+        minNum = Math.min(minNum, num);
+        maxNum= Math.max(maxNum, num);
+       }
+       int range = maxNum-minNum+1;
+       int shift = -minNum;
+       int[] countArr = new int[range];
+       for(int num:arr){
+        countArr[num+shift]=1;
+       }
+       int prev =0;
+       int maxPairDiff = maxNum-minNum;
+       for(int cur=1;cur<=maxNum+shift;++cur){
+         if(countArr[cur]==0) continue;
+         if(cur-prev == maxPairDiff){
+            result.add(List.of(prev-shift, cur-shift));
+         }else if(cur-prev < maxPairDiff){
+            result.clear();
+            maxPairDiff = cur-prev;
+           
+            result.add(List.of(prev-shift, cur-shift));
+         }
+          prev = cur;
+       }
+       return result;
     }
 }
