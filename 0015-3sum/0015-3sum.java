35066f0cfd37b5583cfa500fe21f733dc3c64610
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-26 16:12:41 -0400

    Time: 30 ms (68.87%), Space: 51.6 MB (50.77%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,53 +1,44 @@
 class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    
     public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
         Arrays.sort(nums);
-        return getThreeSum(nums, 0, 3, n, 0);
-    }
-    private List<List<Integer>> getThreeSum(int[] nums, int index, int k, int length, int sum){
-        List<List<Integer>>result = new ArrayList<>();
-        if(index >= length){
-            return result;
-        }
-        if(k==2){
-            int i= index, j = length-1;
-            while(i < j){
-                if(nums[i]+nums[j] == sum){
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    result.add(list);
-                    i++;
-                    j--;
-                    while(i < length-1 && nums[i] == nums[i-1]){
-                        i++;
-                    }
-                    while(j > 0 && nums[j] == nums[j+1]){
-                        j--;
-                    }
-                }else if(nums[i]+nums[j] > sum){
-                    j--;
-                }else{
-                    i++;
-                }
-            }
-
-        }else{
-            for(int i=index;i<length-k+1;i++){
-                List<List<Integer>>ans = getThreeSum(nums, i+1, k-1, length, sum-nums[i]);
-                System.out.println(ans);
-                if(ans != null){
-                    for(List<Integer>lis : ans){
-                        lis.add(0, nums[i]);
-                    }
-                    result.addAll(ans);
-                    while(i < length-1 && nums[i] == nums[i+1]){
-                        i++;
-                    }
-                }
-                
-            }
-        }
+        getThreeSum(nums, 0, 3, n, 0, new ArrayList<>());
         return result;
+    }
+    
+    private void getThreeSum(int[] nums, int index, int k, int length, int sum, List<Integer> temp) {
+        if (k == 2) {
+            int i = index, j = length - 1;
+            while (i < j) {
+                int twoSum = nums[i] + nums[j];
+                if (twoSum == sum) {
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    result.add(new ArrayList<>(temp)); // add a copy
+                    temp.remove(temp.size() - 1); // backtrack
+                    temp.remove(temp.size() - 1); // backtrack
+                    i++;
+                    j--;
+                    while (i < j && nums[i] == nums[i - 1]) i++;
+                    while (i < j && nums[j] == nums[j + 1]) j--;
+                } else if (twoSum < sum) {
+                    i++;
+                } else {
+                    j--;
+                }
+            }
+            return;
+        }
+        
+        for (int i = index; i < length - k + 1; i++) {
+            if (i > index && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            temp.add(nums[i]);
+            getThreeSum(nums, i + 1, k - 1, length, sum - nums[i], temp);
+            temp.remove(temp.size() - 1); // backtrack
+        }
     }
 }
