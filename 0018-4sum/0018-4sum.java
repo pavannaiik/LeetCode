6d5c607932275e2228e86bd07cd92b4936d855e9
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-26 16:18:53 -0400

    Time: 15 ms (86.84%), Space: 45.4 MB (16.08%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,48 +1,40 @@
 class Solution {
-    int len;
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        len = nums.length;
-        Arrays.sort(nums);
-        return kSum(nums, target, 4, 0);
-        
-    }
-    public List<List<Integer>> kSum(int [] nums, long target, int k, int index){
-        List<List<Integer>>ans = new ArrayList<>();
-        if(index >= len){
-            return ans;
-        }
-        if(k==2){
-            int i= index, j = len-1;
-            while(i < j){
-                long sum = nums[i]+nums[j];
-                if(sum == target){
-                    List<Integer>list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    ans.add(list);
-                    while( i < len-1 && nums[i]==nums[i+1]) i++;
-                    while(j >0 && nums[j]==nums[j-1])j--;
-                    i++;
-                    j--;
-                }else if(sum>target){
-                    j--;
-                }else{
-                    i++;
-                }
-            }
-        }else{
-            
-            for(int i=index;i<len-k+1;i++){
-                List<List<Integer>>res = kSum(nums, target-nums[i], k-1, i+1);
-                if(res!=null){
-                    for(List<Integer>list:res){
-                        list.add(0, nums[i]);
-                    }
-                    ans.addAll(res);
-                }
-                while(i < len-1 && nums[i]==nums[i+1]) i++;
+List<List<Integer>> result = new ArrayList<>();
+
+public List<List<Integer>> fourSum(int[] nums, int target) {
+    int n = nums.length;
+    Arrays.sort(nums);
+    getThreeSum(nums, 0, 4, n, target, new ArrayList<>());
+    return result;
+}
+
+private void getThreeSum(int[] nums, int index, int k, int length, long sum, List<Integer> temp) {
+    if (k == 2) {
+        int i = index, j = length - 1;
+        while (i < j) {
+            int twoSum = nums[i] + nums[j];
+            if (twoSum == sum) {
+                result.add(List.of(temp.get(0),temp.get(1),nums[i],nums[j])); 
+                i++;
+                j--;
+                while (i < j && nums[i] == nums[i - 1]) i++;
+                while (i < j && nums[j] == nums[j + 1]) j--;
+            } else if (twoSum < sum) {
+                i++;
+            } else {
+                j--;
             }
         }
-        return ans;
+        return;
     }
+    
+    for (int i = index; i < length - k + 1; i++) {
+        if (i > index && nums[i] == nums[i - 1]) {
+            continue;
+        }
+        temp.add(nums[i]);
+        getThreeSum(nums, i + 1, k - 1, length, sum - nums[i], temp);
+        temp.remove(temp.size() - 1); // backtrack
+    }
+}
 }
