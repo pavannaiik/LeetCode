2e17d81f73745ecd4f9e418edeb8903a536037a8
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-26 19:00:39 -0400

    Time: 2 ms (49.01%), Space: 46 MB (97.32%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,6 +1,11 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[] arr = new int[100001];
+        int maxVal = nums[0];
+        for(int num:nums){
+            if(num <0) num=-1*num;
+            maxVal = Math.max(maxVal, num);
+        }
+        int[] arr = new int[maxVal+1];
         for(int num:nums){
             if(num < 0) num = -1*num;
             arr[num]++;
@@ -8,7 +13,7 @@ class Solution {
         int n = nums.length;
         int[] result = new int[n];
         int k=0;
-        for(int i=0;i<100001;i++){
+        for(int i=0;i<=maxVal;i++){
             if(arr[i] <=0) continue;
             while(arr[i]-- >0){
                 result[k++]=(i)*(i);
