1001077b42e4695366577e2b7a57c1486dbfccd1
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-05-11 13:11:08 -0400

    Time: 4 ms (92.68%), Space: 45.5 MB (67.89%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -10,14 +10,14 @@ public class Codec {
 
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
-        List<String> ret = new ArrayList<String>();
+        List<String> res = new ArrayList<String>();
         int i = 0;
         while(i < s.length()) {
-            int slash = s.indexOf('/', i);
+            int slash = s.indexOf('/',i);
             int size = Integer.valueOf(s.substring(i, slash));
             i = slash + size + 1;
-            ret.add(s.substring(slash + 1, i));
+            res.add(s.substring(slash + 1, i));
         }
-        return ret;
+        return res;
     }
 }
