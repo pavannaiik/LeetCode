98f1b88dcf63e12beae744f4f8c0a142b2e11395
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-05-11 13:14:43 -0400

    Time: 2 ms (78.91%), Space: 44.2 MB (88.56%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,31 +1,27 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                char num = board[row][col];
-                if (num != '.') { 
-                    if (!isValid(board, row, col, num)) 
-                        return false;
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c == '.') continue;
+
+                int num = c - '1';  // Convert character '1'-'9' to index 0-8
+                int boxIndex = (i / 3) * 3 + (j / 3);  // Calculate box index (0-8)
+
+                if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
+                    return false;  // Duplicate found
                 }
+
+                rows[i][num] = true;
+                cols[j][num] = true;
+                boxes[boxIndex][num] = true;
             }
         }
-        return true;
-    }
-    private boolean isValid(char[][] board, int row, int col, char num) {
-        for (int i = 0; i < 9; i++) {
-            if (i != col && board[row][i] == num) 
-                return false;
-            if (i != row && board[i][col] == num)
-                return false;
-        }
-        int startRow = (row / 3) * 3;
-        int startCol = (col / 3) * 3;
-        System.out.println(startRow);
-        System.out.println(startCol);
-        for (int i = startRow; i < startRow + 3; i++) 
-            for (int j = startCol; j < startCol + 3; j++) 
-                if ((i != row || j != col) && board[i][j] == num) 
-                    return false;
+
         return true;
     }
 }
