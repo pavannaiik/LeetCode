b13edbd6b0f0b46c55e8ca93a90615dbc2aee1a6
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-05-11 13:07:47 -0400

    Time: 15 ms (40.54%), Space: 49.1 MB (24.99%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,32 +1,21 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer>freqMap = new HashMap<>();
-        int maxFreq = 0;
         for(int num: nums){
             freqMap.put(num, freqMap.getOrDefault(num,0)+1);
-            maxFreq = Math.max(maxFreq, freqMap.get(num));
         }
-        List<List<Integer>>arrList = new ArrayList<>(maxFreq);
-        for(int i=0;i<maxFreq;i++){
-            arrList.add(new ArrayList<>());
-        }
+        PriorityQueue<Integer>pq = new PriorityQueue<>((a,b)->freqMap.get(a)-freqMap.get(b));
         for(int num:freqMap.keySet()){
-            arrList.get(freqMap.get(num)-1).add(num);
+            pq.add(num);
+            if(pq.size()>k){
+                pq.poll();
+            }
         }
         int[] result = new int[k];
-        int index=0;
-        for(int i=maxFreq-1;i>=0;i--){
-            if(index>=k){
-                break;
-            }
-            for(int currNum: arrList.get(i)){
-                if(index>=k){
-                    break;
-                }
-                result[index++]=currNum;
-            }
+        int i=0;
+        while(!pq.isEmpty()){
+            result[i++]=pq.poll();
         }
         return result;
-        
     }
 }
