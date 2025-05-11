21853dbc84f0b74d04c75a1f02d47e3c3d56eabf
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-05-11 14:07:37 -0400

    Time: 1 ms (99.84%), Space: 43.2 MB (100%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -4,20 +4,16 @@ class Solution {
         this.pos = tokens.length-1;
         return evaluate(tokens);
     }
-
     public int evaluate(String[] t){
         String token = t[pos];
         pos--;
         if(token.equals("+")){
             return evaluate(t) + evaluate(t); 
-        }else
-        if(token.equals("*")){
+        }else if(token.equals("*")){
             return evaluate(t) * evaluate(t);
-        }else 
-        if(token.equals("-")){
+        }else if(token.equals("-")){
             return -evaluate(t) + evaluate(t);
-        }else
-        if(token.equals("/")){
+        }else if(token.equals("/")){
             int right = evaluate(t);
             return evaluate(t) / right;
         }else{
