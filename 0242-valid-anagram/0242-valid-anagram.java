ba60cc5cc77f2f0f9f5eb10dba3bba1183478f5b
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-05-11 13:05:15 -0400

    Time: 3 ms (90.67%), Space: 42.7 MB (97.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,19 +4,16 @@ class Solution {
         if (s.length() != t.length()) {
             return false;
         }
-         int[] arr = new int[26];
-
+        int[] table = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            arr[s.charAt(i) - 'a']++;
-            arr[t.charAt(i) - 'a']--;
+            table[s.charAt(i) - 'a']++;
         }
-
-        for (int val : arr) {
-            if (val != 0) {
+        for (int i = 0; i < t.length(); i++) {
+            table[t.charAt(i) - 'a']--;
+            if (table[t.charAt(i) - 'a'] < 0) {
                 return false;
             }
         }
-
         return true;
             
         }
