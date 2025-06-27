f3b76d985d59eb5848e3652f6ccb02535f8c38e3
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-06-26 23:51:42 -0400

    Time: 10 ms (69.41%), Space: 44.6 MB (82.79%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -5,7 +5,7 @@ class Solution {
         for(int right=0;right<s.length();right++){
             maxCount= Math.max(maxCount, ++count[s.charAt(right)-'A']);
             
-            while(right-left+1 - maxCount > k){
+            if(right-left+1 - maxCount > k){
                 count[s.charAt(left)-'A']--;
                 left++;
             }
