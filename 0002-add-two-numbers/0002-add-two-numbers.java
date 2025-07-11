d0ba3cf4388adadb22ae6e4de368ce63597ddcaa
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-10 21:49:23 -0400

    Time: 1 ms (100%), Space: 44.5 MB (68.21%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,35 +10,33 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode ans = new ListNode(0);
-        ListNode prev = ans;
-        int carry=0;
-        while(l1!=null && l2!=null){
-            int sum = l1.val + l2.val + carry;
-            carry = sum/10;
-            ans.next = new ListNode(sum%10);
-            ans = ans.next;
+        ListNode temp = new ListNode(0);
+        ListNode prev = temp;
+        int carry =0;
+        while(l1 != null && l2 != null){
+            int curSum =  carry + l1.val + l2.val;
             l1=l1.next;
             l2=l2.next;
+            carry = curSum/10;
+            temp.next = new ListNode(curSum%10);
+            temp = temp.next;
         }
         while(l1!=null){
-             int sum = l1.val  + carry;
-             carry = sum/10;
-             ans.next = new ListNode(sum%10);
-             ans = ans.next;
-             l1=l1.next;
+            int curSum =  carry + l1.val ;
+            l1=l1.next;
+            carry = curSum/10;
+            temp.next = new ListNode(curSum%10);
+            temp = temp.next;
         }
         while(l2!=null){
-             int sum = l2.val  + carry;
-             carry = sum/10;
-             ans.next = new ListNode(sum%10);
-             ans = ans.next;
-             l2=l2.next;
+            int curSum =  carry + l2.val ;
+            l2=l2.next;
+            carry = curSum/10;
+            temp.next = new ListNode(curSum%10);
+            temp = temp.next;
         }
-        if(carry!=0){
-            ans.next = new ListNode(carry);
-            ans = ans.next;
-        }
+        if(carry > 0) temp.next = new ListNode(carry);
+        temp = temp.next;
         return prev.next;
     }
 }
