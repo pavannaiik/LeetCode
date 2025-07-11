1230a307456b1bf3a63f03d99f585012daaced18
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-10 22:21:35 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,29 +1,30 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if(nums1.length>nums2.length){
+        int n = nums1.length, m = nums2.length;
+        if(m < n){
             return findMedianSortedArrays(nums2, nums1);
         }
-        int n=nums1.length, m = nums2.length;
-        int i=0, j=n;
-        while(i<=j){
-            int partX = i+(j-i)/2;
+        int i =0 , j = n;
+        while( i <= j){
+            int partX = i+ (j-i)/2;
             int partY = (m+n+1)/2-partX;
-            int maxX = partX==0?Integer.MIN_VALUE:nums1[partX-1];
-            int minX = partX==n?Integer.MAX_VALUE:nums1[partX];
-            int maxY = partY==0?Integer.MIN_VALUE:nums2[partY-1];
-            int minY = partY==m?Integer.MAX_VALUE:nums2[partY];
-            if(maxX <= minY && maxY <= minX){
-                if((n+m)%2==0){
-                    return (double) (Math.max(maxX,maxY)+Math.min(minX,minY))/2.0;
+            int partXMax = (partX==0)? Integer.MIN_VALUE: nums1[partX-1];
+            int partXMin = (partX==n)?Integer.MAX_VALUE:nums1[partX];
+            int partYMax = (partY==0)? Integer.MIN_VALUE: nums2[partY-1];
+            int partYMin = (partY==m)?Integer.MAX_VALUE:nums2[partY];
+            if(partXMin > partYMax && partYMax < partXMin){
+                if((m+n)%2==0){
+                    return (double)(Math.min(partXMin, partYMin) + Math.max(partXMax, partYMax))/2.0;
                 }else{
-                    return (double) Math.max(maxX,maxY);
+                    return (double)(Math.max(partXMax, partYMax));
                 }
-            }else if(minX > maxY){
+            }else if(partXMin > partYMax){
                 j = partX-1;
             }else{
                 i= partX+1;
             }
+
         }
-        return -1;
+        return 0.0;
     }
 }
