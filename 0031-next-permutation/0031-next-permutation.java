4887644b5e604ee97cc87ce8e194dffa3a971ca5
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-12 18:40:59 -0400

    Time: 0 ms (100%), Space: 43.2 MB (56.29%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,42 +1,37 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-       int n = nums.length;
-        int i = n - 2;
-
-        // Step 1: Find the pivot
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
+        int n = nums.length;
+        int i = n-2;
+        //find pivot
+        while(i>=0 && nums[i]>=nums[i+1]){
             i--;
         }
-
-        if (i >= 0) {
-            // Step 2: Find the successor
-            int j = n - 1;
-            while (nums[j] <= nums[i]) {
+        if( i>=0){
+            //find next greater element than pivot which is a successor
+            int j = n-1;
+            while(nums[j]<=nums[i]){
                 j--;
             }
-
-            // Step 3: Swap the pivot with the successor
+            //swap them
             swap(nums, i, j);
+
         }
+        //reverse elements after successor
+        reverse(nums, i+1, nums.length-1);
 
-        // Step 4: Reverse the suffix
-        reverse(nums, i + 1, n - 1);
     }
-
-    // Helper method to swap elements in the array
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    // Helper method to reverse a subarray
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            swap(nums, start, end);
-            start++;
-            end--;
+    public void reverse(int[] nums, int i, int j){
+        while(i < j){
+            int temp = nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
         }
     }
-
+    public void swap(int[] nums, int i, int j){
+            int temp = nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+    }
 }
