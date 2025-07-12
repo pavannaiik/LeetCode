ddf9bf3542b6b89d94389d0fdc3d923ecac2a29a
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-12 12:43:48 -0400

    Time: 31 ms (56.85%), Space: 52.5 MB (22.14%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,42 +1,42 @@
 class Solution {
-    List<List<Integer>> result = new ArrayList<>();
-    
     public List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length;
         Arrays.sort(nums);
-        getThreeSum(nums, 0, 3, n, 0, new ArrayList<>());
-        return result;
+        return getKSum(nums, 0, 0, 3);
     }
-    
-    private void getThreeSum(int[] nums, int index, int k, int length, int sum, List<Integer> temp) {
-        if (k == 2) {
-            int i = index, j = length - 1;
-            while (i < j) {
-                int twoSum = nums[i] + nums[j];
-                if (twoSum == sum) {
-                    List<Integer>comb = new ArrayList<>(temp);
-                    comb.add(nums[i]);
-                    comb.add(nums[j]);
-                    result.add(comb); 
-                    i++;
-                    j--;
-                    while (i < j && nums[i] == nums[i - 1]) i++;
-                    while (i < j && nums[j] == nums[j + 1]) j--;
-                } else if (twoSum < sum) {
-                    i++;
-                } else {
-                    j--;
+    public List<List<Integer>> getKSum(int[] nums, int index, int target, int k){
+        List<List<Integer>> answer = new ArrayList<>();
+        if(index >= nums.length){
+         return answer;
+        }
+        if(k==2){
+           int left = index, right = nums.length-1;
+           while(left < right){
+             if(nums[left]+nums[right] == target){
+             List<Integer>tempList = new ArrayList<>();
+             tempList.add(nums[left]);
+             tempList.add(nums[right]);
+             answer.add(tempList);
+             while(left<nums.length-1 && nums[left] == nums[left+1]) left++;
+             while(right>0 && nums[right]==nums[right-1]) right--;
+             left++;
+             right--;
+             }else if(nums[left]+nums[right] > target){
+             right--;
+             }else{
+             left++;
+             }
+           }
+        }else{
+           for(int i=index; i< nums.length-k+1;i++){
+                List<List<Integer>>curList = getKSum(nums, i+1, target-nums[i], k-1);
+                for(List<Integer>list: curList){
+                 list.add(0, nums[i]);
                 }
-            }
-            return;
+                answer.addAll(curList);
+                while(i<nums.length-1 && nums[i] == nums[i+1]) i++;
+
+           }
         }
-        for (int i = index; i < length - k + 1; i++) {
-            if (i > index && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            temp.add(nums[i]);
-            getThreeSum(nums, i + 1, k - 1, length, sum - nums[i], temp);
-            temp.remove(temp.size() - 1); // backtrack
-        }
+        return answer;
     }
 }
