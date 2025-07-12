156c1874c25815b60f492a758f3fbf42c3e6de0d
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-12 16:23:55 -0400

    Time: 3 ms (99.08%), Space: 45.5 MB (85.38%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -1,23 +1,34 @@
 public class Codec {
-	// Encodes a list of strings to a single string.
+
+    // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
         StringBuilder sb = new StringBuilder();
-        for(String s : strs) {
-            sb.append(s.length()).append('/').append(s);
+        for(String s: strs ){
+            sb.append(s.length());
+            sb.append('#');
+            sb.append(s);
         }
         return sb.toString();
     }
 
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
-        List<String> res = new ArrayList<String>();
-        int i = 0;
-        while(i < s.length()) {
-            int slash = s.indexOf('/',i);
-            int size = Integer.valueOf(s.substring(i, slash));
-            i = slash + size + 1;
-            res.add(s.substring(slash + 1, i));
+        List<String>ans = new ArrayList<>();
+        int i=0;
+        int n = s.length();
+        while(i < n){
+            int num = 0;
+            while(s.charAt(i)!='#'){
+                num = num*10 + s.charAt(i)-'0';
+                i++;
+            }
+            ans.add(s.substring(i+1, i+num+1));
+            i = i+num+1;
         }
-        return res;
+        return ans;
     }
 }
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
