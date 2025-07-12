cd91d22a3aad91a89040435ce2326c501735fc6b
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-12 10:44:31 -0400

    Time: 0 ms (100%), Space: 57.8 MB (13.71%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-       int n = nums.length;
+        int n = nums.length;
         k = k % nums.length;
         swapNums(nums, 0, n-1);
         swapNums(nums, 0, k-1);
