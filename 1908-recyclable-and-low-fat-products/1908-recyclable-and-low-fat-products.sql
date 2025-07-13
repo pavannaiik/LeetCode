803ef0f357970cce390e05096db67684c940f4b8
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-13 02:14:28 -0400

    Time: 528 ms (23.35%), Space: 0B (100%) - LeetHub

--- 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
@@ -1,3 +1,3 @@
-# Write your MySQL query statement below
-select product_id from products
+/* Write your T-SQL query statement below */
+select product_id from products(nolock)
 where low_fats = 'Y' and recyclable = 'Y';
