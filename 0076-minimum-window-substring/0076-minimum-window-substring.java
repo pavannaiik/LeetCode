5c4093c4e99af8e994d689854d5ba635b4eb5b69
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-13 23:52:01 -0400

    Time: 2 ms (99.86%), Space: 45 MB (67.27%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -7,7 +7,7 @@ class Solution {
         int required = t.length();
         int left =0;
         int curMin = Integer.MAX_VALUE;
-        StringBuilder sb = new StringBuilder();
+        int start =0, end =0;
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
             charArray[ch]--;
@@ -17,13 +17,14 @@ class Solution {
             while(required == 0){
                 if(curMin > i-left+1){
                     curMin = i-left+1;
-                    sb = new StringBuilder(s.substring(left, i+1));
+                    start = left;
+                    end = i;
                 }
                 char leftChar = s.charAt(left);
                 left++;
                 if(charArray[leftChar]++ == 0) required++;
             }
         }
-        return curMin == Integer.MAX_VALUE? "":sb.toString();
+        return curMin == Integer.MAX_VALUE? "":s.substring(start, end+1);
     }
 }
