7d295ae3b91c923606ab40e02aa8fc4ae4b04766
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-13 23:25:51 -0400

    Time: 79 ms (37.55%), Space: 61.7 MB (5.04%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,25 +1,24 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         Stack<Pair<Integer,Integer>>stack = new Stack<>();
-        int max_area=0;
+        int max_area =0;
         int n = heights.length;
-        for(int i=0;i<heights.length;i++){
-            int min_index=i;
-            while(!stack.isEmpty() && stack.peek().getValue() > heights[i]){
-                Pair<Integer,Integer> p = stack.pop();
-                int index = p.getKey();
-                int height=p.getValue();
-                max_area = Math.max(max_area, (i-index)*height);
-                min_index=Math.min(min_index, index);
+        for(int i=0;i<n;i++){
+            int min_index = i;
+            while(!stack.isEmpty() && stack.peek().getKey() > heights[i]){
+                max_area = Math.max(max_area, stack.peek().getKey() * (i-stack.peek().getValue()));
+                min_index = Math.min(min_index, stack.peek().getValue());
+                stack.pop();
             }
-            stack.add(new Pair<>(min_index, heights[i]));
+            
+            stack.add(new Pair<>(heights[i], min_index));
         }
         while(!stack.isEmpty()){
-            Pair<Integer,Integer> p = stack.pop();
-            int index = p.getKey();
-            int height=p.getValue();
-            max_area = Math.max(max_area, (n-index)*height);
+            max_area = Math.max(max_area, stack.peek().getKey() * ( n- stack.peek().getValue()));
+            stack.pop();
         }
         return max_area;
     }
 }
+// 2 -> 2 ->
+// 2,0 -> 1, 0 -> 5, 2, 6, 3,
