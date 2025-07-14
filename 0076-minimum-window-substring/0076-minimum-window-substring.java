488096ac1cfbd338412f0d5c1e993a1cc468ddb6
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-13 23:49:47 -0400

    Time: 345 ms (5.04%), Space: 45.7 MB (21.08%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,26 +1,29 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int n = s.length();
-        int[] charMap = new int[123];
-        for(char c : t.toCharArray()){
-            charMap[c]++;
+        int[] charArray = new int[123];
+        for(char ch: t.toCharArray()){
+            charArray[ch]++;
         }
-        int right =0;
-        int minLen = Integer.MAX_VALUE;
-        int left =0, start =0;
         int required = t.length();
-        while( right < n){
-            char rightChar = s.charAt(right++);
-            if(charMap[rightChar]-- > 0) required--;
-            while(required ==0){
-                if(right - left < minLen){
-                    minLen = right - left;
-                    start = left;
+        int left =0;
+        int curMin = Integer.MAX_VALUE;
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            charArray[ch]--;
+            if(charArray[ch]>=0){
+                required--;
+            }
+            while(required == 0){
+                if(curMin > i-left+1){
+                    curMin = i-left+1;
+                    sb = new StringBuilder(s.substring(left, i+1));
                 }
-                int leftChar = s.charAt(left++);
-                if(charMap[leftChar]++ ==0) required++;
+                char leftChar = s.charAt(left);
+                left++;
+                if(charArray[leftChar]++ == 0) required++;
             }
         }
-        return minLen == Integer.MAX_VALUE?"": s.substring(start, start+minLen);
+        return curMin == Integer.MAX_VALUE? "":sb.toString();
     }
 }
