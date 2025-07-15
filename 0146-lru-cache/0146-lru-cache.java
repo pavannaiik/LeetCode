f8f725097357b32e259a30270a553131addf77dc
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-15 18:29:26 -0400

    Time: 50 ms (34.17%), Space: 114.9 MB (51.14%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,58 +1,70 @@
 class LRUCache {
-    int size;
-    Map<Integer, Node>map = new HashMap<>();
-    Node head, tail;
+    HashMap<Integer, Node>nodeMap;
+    Node front;
+    Node rear;
+    int size ;
     public LRUCache(int capacity) {
-        size=capacity;
-        head=new Node(-1, -1);
-        tail=new Node(-1, -1);
-        head.next=tail;
-        tail.prev=head;
+        this.nodeMap = new HashMap<>();
+        this.rear = new Node(-1, -1);
+        this.front = new Node(-1, -1);
+        this.size = capacity;
+        rear.prev = front;
+        front.next = rear;
     }
     
     public int get(int key) {
-        if(!map.containsKey(key)){
+        if(nodeMap.containsKey(key)){
+            Node cur = nodeMap.get(key);
+            //when a key accessed it should be 
+            //removed from the existing position and put in the front
+            removeNodeFromLinkedList(cur);
+            addNodeFront(cur);
+            return cur.val;
+        }else{
             return -1;
         }
-        Node cur = map.get(key);
-        removeNode(cur);
-        insertNode(cur);
-        return cur.value;
     }
-
-    public void removeNode(Node cur){
-        map.remove(cur.key);
-        cur.prev.next = cur.next;
+    public void removeNodeFromLinkedList(Node cur){
         cur.next.prev = cur.prev;
+        cur.prev.next = cur.next;
+        nodeMap.remove(cur.key);
     }
-    public void insertNode(Node cur){
-        map.put(cur.key, cur);
-        Node nextHead = head.next;
-        head.next = cur;
-        cur.prev= head;
-        cur.next = nextHead;
-        nextHead.prev= cur;
+    public void addNodeFront(Node cur){
+        Node frontNext = front.next;
+        cur.next = frontNext;
+        frontNext.prev = cur;
+        front.next = cur;
+        cur.prev = front;
 
+        nodeMap.put(cur.key, cur);
     }
     
     public void put(int key, int value) {
-        Node curNode = new Node(key,value);
-        if(map.containsKey(key)){
-            removeNode(map.get(key));
+        
+        if(nodeMap.size()  > 1 && nodeMap.containsKey(key)){
+            removeNodeFromLinkedList(nodeMap.get(key));
+        }else if(nodeMap.size() >= size){
+            //remove from the end
+            removeNodeFromEnd();
         }
-         if(map.size()==size){
-            removeNode(tail.prev);
-        }
-        insertNode(curNode);
+        Node newNode = new Node(key, value);
+        addNodeFront(newNode);
+    }
+    public void removeNodeFromEnd(){
+        Node rearPrev = rear.prev;
+        rearPrev.prev.next = rear;
+        rear.prev = rearPrev.prev;
+        nodeMap.remove(rearPrev.key);
     }
 }
-
-class Node{
-    int key, value;
-    Node next, prev;
-    Node(int key, int value){
-        this.key=key;
-        this.value=value;
+class Node {
+    int key;
+    int val;
+    Node prev;
+    Node next;
+    Node(int key, int val){
+        this.key = key;
+        this.val = val;
     }
 }
 /**
