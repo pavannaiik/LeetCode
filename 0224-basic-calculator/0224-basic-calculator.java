5147a8cf0a0458e8af8ff45e8c6c84bb6cc0bf8a
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-16 22:01:59 -0400

    Time: 9 ms (59.51%), Space: 43.2 MB (98.78%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,24 +1,23 @@
 class Solution {
     public int calculate(String s) {
         int result =0;
-        Stack<Integer>stack = new Stack<>();
         int i=0, j= s.length();
-        int num =0;
+        int num=0;
+        Stack<Integer>stack = new Stack<>();
         int sign =1;
         stack.push(sign);
         while(i < j){
             char ch = s.charAt(i);
             if(Character.isDigit(ch)){
-               num = num*10 + ch-'0';
-            }else if( ch =='+' || ch=='-'){
+                num = num*10 + ch - '0';
+            }else if(ch == '+' || ch == '-'){
                 result += sign * num;
                 sign = stack.peek() * ((ch=='+')?1:-1);
-                num=0;
-            }else if(ch=='('){
-                stack.push(sign);
-            }else if(ch==')') stack.pop();
+                num = 0;
+            }else if(ch == '(') stack.push(sign);
+            else if(ch == ')') stack.pop();
             i++;
         }
-        return result + sign * num;
+        return result + (num * sign);
     }
 }
