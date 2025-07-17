9d04341495a79d847082fa83b1ad961636ecef39
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-16 21:38:55 -0400

    Time: 3 ms (99.8%), Space: 45.8 MB (40.92%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.java
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.java
@@ -1,26 +1,24 @@
 class TicTacToe {
-    private int[] rows ;
-    private int[] cols ;
-    private int diagonal =0;
-    private int antiDiagonal =0;
+    int[] rows;
+    int[] cols;
+    int diagonal ;
+    int antidiagonal;
     public TicTacToe(int n) {
-        rows = new int[n];
-        cols = new int[n];
+        this.rows = new int[n];
+        this.cols = new int[n];
+        this.diagonal = 0;
+        this.antidiagonal =0;
     }
     
     public int move(int row, int col, int player) {
-        //put player in the given place
-        int currentPlayer = player == 1? 1:-1;
-        rows[row]+=currentPlayer;
-        cols[col]+=currentPlayer;
-        if(row==col){
-            diagonal+=currentPlayer;
-        }
-        if(col == (cols.length-row-1)){
-            antiDiagonal+=currentPlayer;
-        }
+        int curPlayer = player==1?1:-1;
+        rows[row] += curPlayer;
+        cols[col] += curPlayer;
+        if(row == col) diagonal += curPlayer;
+        if(col == (cols.length-row-1)) antidiagonal += curPlayer;
         int n = rows.length;
-        if(Math.abs(rows[row])==n || Math.abs(cols[col])==n || Math.abs(diagonal)==n || Math.abs(antiDiagonal)==n){
+        if(Math.abs(rows[row])==n || Math.abs(cols[col])==n 
+        || Math.abs(antidiagonal)==n || Math.abs(diagonal)==n){
             return player;
         }
         return 0;
