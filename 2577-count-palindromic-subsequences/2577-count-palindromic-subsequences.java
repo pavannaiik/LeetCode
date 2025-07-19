e253a8fea287346f2cd5f3df48a1e066074b0292
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-19 11:10:38 -0400

    Time: 17 ms (95%), Space: 41.8 MB (96.25%) - LeetHub

--- 2577-count-palindromic-subsequences/2577-count-palindromic-subsequences.java
+++ 2577-count-palindromic-subsequences/2577-count-palindromic-subsequences.java
@@ -1,46 +1,37 @@
 class Solution {
     public int countPalindromes(String s) {
-        int mod = 1_000_000_007;
-        int n = s.length();
         int[][] prefix = new int[10][10];
         int[][] suffix = new int[10][10];
         int[] left = new int[10];
         int[] right = new int[10];
-
-        // Count suffix pairs
-        for (int i = n - 1; i >= 0; i--) {
-            int d = s.charAt(i) - '0';
-            for (int j = 0; j < 10; j++) {
+        long result = 0;
+        int mod = 1_000_000_007;
+        int n = s.length();
+        for(int i=n-1;i>=0;i--){
+            int d = s.charAt(i)-'0';
+            for(int j=0;j<10;j++){
                 suffix[d][j] += right[j];
             }
             right[d]++;
         }
-
-        long result = 0;
-
-        for (int i = 0; i < n; i++) {
-            int mid = s.charAt(i) - '0';
-
-            // Remove current from suffix
+        for(int i=0;i<n;i++){
+            //first remobve  from suffix
+            int mid = s.charAt(i)-'0';
             right[mid]--;
-            for (int j = 0; j < 10; j++) {
+            for(int j=0;j<10;j++){
                 suffix[mid][j] -= right[j];
             }
-
-            // Count combinations of form a b c b a
-            for (int a = 0; a < 10; a++) {
-                for (int b = 0; b < 10; b++) {
-                    result = (result + 1L * prefix[a][b] * suffix[b][a]) % mod;
+            for(int a=0;a<10;a++){
+                for(int b=0;b<10;b++){
+                    result = (result + 1L * prefix[a][b] * suffix[b][a])%mod;
                 }
             }
-
-            // Update prefix
-            for (int j = 0; j < 10; j++) {
+            //add to prefix
+            for(int j=0;j<10;j++){
                 prefix[j][mid] += left[j];
             }
             left[mid]++;
         }
-
-        return (int) result;
+        return (int)result;
     }
 }
