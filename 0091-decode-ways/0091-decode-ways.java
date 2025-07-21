4a54278e8e348994c010ef4ad8aeb655f93e2817
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-20 21:18:01 -0400

    Time: 0 ms (100%), Space: 41.6 MB (87.12%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -2,20 +2,22 @@ class Solution {
     public int numDecodings(String s) {
         int n = s.length();
         if(s.charAt(0)=='0') return 0;
-        int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
+        int dp1 = 1;
+        int dp2 = 1;
         for(int i=2;i<=n;i++){
             int prev = s.charAt(i-2)-'0';
             int curNum = s.charAt(i-1)-'0';
+            int dp = 0;
             if (curNum != 0) {
-                dp[i] += dp[i - 1];
+                dp += dp1;
             }
             if(prev == 1 && curNum <= 9  || prev == 2 && curNum <=6){
-                dp[i] += dp[i-2];
+                dp += dp2;
             }
+            dp2 = dp1;
+            dp1 = dp;
             
         }
-        return dp[n];
+        return dp1;
     }
 }
