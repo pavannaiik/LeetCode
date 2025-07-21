43a1fa4997ce9ae1c03c1a98ba1a35511382192e
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-21 18:36:39 -0400

    Time: 13 ms (50.18%), Space: 49.2 MB (29.08%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -1,36 +1,35 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         HashMap<Integer, List<int[]>> map = new HashMap<>();
-		for(int[] time: times){
-			int u = time[0], v = time[1], w = time[2];
-			if(!map.containsKey(u)){
-				map.put(u, new ArrayList<>());
-			}
-			map.get(u).add(new int[]{v, w});
-		}
-		int[] dist = new int[n+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[k] = 0;
-        PriorityQueue<int[]>pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
-        pq.add(new int[]{k,0});
+        for(int[] time: times){
+            if(!map.containsKey(time[0])){
+                map.put(time[0], new ArrayList<>());
+            }
+            map.get(time[0]).add(new int[]{time[1], time[2]});
+        }
+        int[] distance = new int[n+1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
+        distance[k]=0;
+        pq.add(new int[]{k, 0});
         while(!pq.isEmpty()){
-            int[] cur = pq.poll();
-            int node = cur[0], time = cur[1];
+            int[] curNode = pq.poll();
+            int node = curNode[0], time = curNode[1];
             if(!map.containsKey(node)) continue;
             for(int[] neighbor: map.get(node)){
                 int next = neighbor[0], weight = neighbor[1];
-                if(dist[next] > weight +time){
-                    dist[next] = weight + time;
-                    pq.add(new int[]{next, dist[next]});
+                if(distance[next] > time + weight){
+                    distance[next]= time+weight;
+                    pq.add(new int[]{next, distance[next]});
                 }
             }
         }
-        int ans = 0;
+        int minTime = 0;
         for(int i=1;i<=n;i++){
-            if(dist[i]==Integer.MAX_VALUE) return -1;
-            ans = Math.max(ans, dist[i]);
+            if(distance[i]== Integer.MAX_VALUE) return -1;
+            minTime = Math.max(minTime, distance[i]);
         }
-		return ans;
-		
+        return minTime;
+
     }
 }
