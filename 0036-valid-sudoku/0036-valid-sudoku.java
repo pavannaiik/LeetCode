d3ad7d5afb65c3cea3be1791a7173fed495950e0
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-22 14:27:58 -0400

    Time: 1 ms (99.96%), Space: 44.3 MB (89.1%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,24 +1,19 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] rows = new boolean[9][9];
-        boolean[][] cols = new boolean[9][9];
-        boolean[][] boxes = new boolean[9][9];
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                char c = board[i][j];
-                if (c == '.') continue;
-
-                int num = c - '1';  // Convert character '1'-'9' to index 0-8
-                int boxIndex = (i / 3) * 3 + (j / 3);  // Calculate box index (0-8)
-
-                if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
-                    return false;  // Duplicate found
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] box = new boolean[9][9];
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.') continue;
+                int digit = board[i][j] - '1';
+                int boxIndex = (i/3) *3 + j/3;
+                if(row[i][digit] || col[j][digit] || box[boxIndex][digit]){
+                    return false;
                 }
-
-                rows[i][num] = true;
-                cols[j][num] = true;
-                boxes[boxIndex][num] = true;
+                row[i][digit] = true;
+                col[j][digit] = true;
+                box[boxIndex][digit] = true;
             }
         }
         return true;
