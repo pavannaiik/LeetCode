25c4e52a10e532bd769a525a4b4e4719dc7089eb
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-22 19:11:07 -0400

    Time: 83 ms (13.37%), Space: 63.5 MB (5.17%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,32 +1,15 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-
-        int n = temperatures.length;
-        
-        int hottest = 0;
-        
-        int answer[] = new int[n];
-        
-        for (int currDay = n - 1; currDay >= 0; currDay--) {
-
-            int currentTemp = temperatures[currDay];
-
-            // hottest temp seen so far moving from the right
-            if (currentTemp >= hottest) {
-                hottest = currentTemp;
-                continue;
+        Stack<Integer>stack = new Stack<>();
+        int[] arr = new int[temperatures.length];
+        Arrays.fill(arr,0);
+        for(int i=0;i<temperatures.length;i++){
+            while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()]){
+                arr[stack.peek()] = i - stack.peek();
+                stack.pop();
             }
-            
-            int days = 1;
-
-            while (temperatures[currDay + days] <= currentTemp) {
-                // Use information from answer to search for the next warmer day
-                days += answer[currDay + days];
-            }
-
-            answer[currDay] = days;
+            stack.push(i);
         }
-        
-        return answer;
+        return arr;
     }
 }
