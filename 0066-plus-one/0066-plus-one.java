1e32598087255800871384bb24405674202b0720
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-21 23:40:18 -0400

    Time: 0 ms (100%), Space: 41.8 MB (66.77%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for(int i=digits.length-1;i>=0 ;i--){
-            if(digits[i]<9){
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i] < 9){
                 digits[i]++;
                 return digits;
             }
             digits[i]=0;
         }
-        //if the program arrived till this point means the we need to add 1 to the start of array
         digits = new int[digits.length+1];
-            digits[0]=1;
+        digits[0]=1;
         return digits;
     }
-    
 }
