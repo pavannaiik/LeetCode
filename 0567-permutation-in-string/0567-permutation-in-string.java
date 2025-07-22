3aed43ab95ae5470a55e23bd38c18c323f037c72
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-22 17:59:34 -0400

    Time: 5 ms (96.12%), Space: 43 MB (50.11%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,28 +1,30 @@
-public class Solution {
+class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int len1 = s1.length(), len2 = s2.length();
-        if (len1 > len2) return false;
-        
-        int[] count = new int[26];
-        for (int i = 0; i < len1; i++) {
-            count[s1.charAt(i) - 'a']++;
-            count[s2.charAt(i) - 'a']--;
+        int[] s1Freq = new int[26];
+        int[] s2Freq = new int[26];
+        int left = 0;
+        int n = s1.length(), m = s2.length();
+        for(char c: s1.toCharArray()) s1Freq[c-'a']++;
+        for(int right = 0;right <m ;right++){
+            s2Freq[s2.charAt(right)-'a']++;
+            if(right-left+1 > n){
+                s2Freq[s2.charAt(left)-'a']--;
+                left++;
+            }
+            if(right-left+1 == n){
+                int flag = 0;
+                for(int i=0;i<26;i++){
+                    if(s1Freq[i]!=s2Freq[i]) 
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if(flag==0) return true;
+
+            }
+
         }
-        if (allZero(count)) return true;
-        
-        for (int i = len1; i < len2; i++) {
-            count[s2.charAt(i) - 'a']--;
-            count[s2.charAt(i - len1) - 'a']++;
-            if (allZero(count)) return true;
-        }
-        
         return false;
-    }
-    
-    private boolean allZero(int[] count) {
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) return false;
-        }
-        return true;
     }
 }
