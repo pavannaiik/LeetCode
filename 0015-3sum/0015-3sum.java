b3decbfc6a881361c09547c87d48bc7b8372192d
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-07-22 14:50:27 -0400

    Time: 30 ms (69.88%), Space: 52.2 MB (31.12%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,41 +1,43 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        return getKSum(nums, 0, 0, 3);
+        return kSum(nums, 0, 0, nums.length, 3);
     }
-    public List<List<Integer>> getKSum(int[] nums, int index, int target, int k){
+    public List<List<Integer>> kSum(int[] nums, int index, int target, int n, int k){
         List<List<Integer>> answer = new ArrayList<>();
-        if(index >= nums.length){
-         return answer;
+        if(index >= n){
+            return answer;
         }
         if(k==2){
-           int left = index, right = nums.length-1;
-           while(left < right){
-             if(nums[left]+nums[right] == target){
-             List<Integer>tempList = new ArrayList<>();
-             tempList.add(nums[left]);
-             tempList.add(nums[right]);
-             answer.add(tempList);
-             while(left<nums.length-1 && nums[left] == nums[left+1]) left++;
-             while(right>0 && nums[right]==nums[right-1]) right--;
-             left++;
-             right--;
-             }else if(nums[left]+nums[right] > target){
-             right--;
-             }else{
-             left++;
-             }
-           }
-        }else{
-           for(int i=index; i< nums.length-k+1;i++){
-                List<List<Integer>>curList = getKSum(nums, i+1, target-nums[i], k-1);
-                for(List<Integer>list: curList){
-                 list.add(0, nums[i]);
+            int i = index, j = n-1;
+            while(i < j){
+                if(nums[i]+nums[j] == target){
+                    List<Integer>newList = new ArrayList<>();
+                    newList.add(nums[i]);
+                    newList.add(nums[j]);
+                    answer.add(newList);
+                    while(i < n-1 && nums[i]==nums[i+1]) i++;
+                    while(j >0 && nums[j] == nums[j-1]) j--;
+                    i++;
+                    j--;
+                }else if(nums[i]+nums[j] < target){
+                    i++;
+                }else{
+                    j--;
                 }
-                answer.addAll(curList);
-                while(i<nums.length-1 && nums[i] == nums[i+1]) i++;
-
-           }
+            }
+        }else{
+            for(int i= index;i<n-k+1;i++){
+                List<List<Integer>>cur = kSum(nums, i+1, target-nums[i], n, k-1);
+                if(cur!=null){
+                    for(List<Integer>list: cur){
+                        list.add(0, nums[i]);
+                    }
+                    answer.addAll(cur);
+                }
+                
+                while(i < n-1 && nums[i]==nums[i+1]) i++;
+            }
         }
         return answer;
     }
